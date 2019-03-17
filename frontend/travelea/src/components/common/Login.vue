6d20aad8a7e8a8fceb73b195db841213a4752f61
Tiger Huang <rsh134@uclive.ac.nz>
2019-03-18 01:04:16 +1300

    Added login page and a login repository #implement

--- frontend/travelea/src/components/common/Login.vue
+++ frontend/travelea/src/components/common/Login.vue
@@ -1,7 +1,8 @@
 <template>
-    <v-form>
-      <v-container>
-        <v-layout align-center justify-center row fill-height/>
+    <div class="width-for-container">
+        <v-form ref="form">
+          <v-container grid-list-xl v-if="showAddDestination">
+            <v-layout align-center justify-center row fill-height/>
                 <v-flex xs12 sm6 md3>
                     <v-text-field
                     v-model="user.email"
@@ -15,26 +16,45 @@
                     label="Password"
                     ></v-text-field>
                 </v-flex>
-        
-                <v-btn round color="primary">Login</v-btn v-on:click.login>
-        </v-layout>
-      </v-container>
-    </v-form>
+
+                <v-btn round v-on:click.login color="primary">Login</v-btn>
+            </v-layout>
+          </v-container>
+        </v-form>
+     </div>
 </template>
 
+<style>
+@import url("https://fonts.googleapis.com/css?family=Karla:400,700");
+
+.outer-container {
+  text-align: center;
+}
+
+.width-for-container {
+  width: 60%;
+}
+</style>
 
 <script>
 
-import { attemptLogin } from "../../repository/LoginRepository";
+import {attemptLogin} from "../../repository/LoginRepository";
 import {RepositoryFactory} from "../../repository/RepositoryFactory"
-import { store } from "../../store/index";
+import {store} from "../../store/index";
 
 export default {
+    store,
+    data() {
+        return {
+          destination: {}
+        };
+      },
     methods: {
-    attemptLogin: function {
-    attemptLogin (this.user).then(() => {
-    this
-    }
-    }
+        attemptLogin: function() {
+            attemptLogin (this.user).then(() => {
+                this.$refs.form.reset();
+                })
+        }
     }
 }
+</script>
