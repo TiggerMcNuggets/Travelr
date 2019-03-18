1ffed061f7cfa7d2069c1de5735ee44ab508afe3
Lorenzo Fasano <fasano.lorenzo96@gmail.com>
2019-03-19 12:48:04 +1300

    Create trip completed process cycle
    - Connected to backend api call
    - Added initial validation to name of destinations added
    - Created submit, reset and add more destinations functionality

--- frontend/travelea/src/components/trips/CreateTrips.vue
+++ frontend/travelea/src/components/trips/CreateTrips.vue
@@ -1,239 +1,192 @@
 /* eslint-disable */
 
-<template>
-  <div class="width-for-container">
-    <v-form ref="form" lazy-validation>
-      <v-container grid-list-xl>
-        <h4>Create a new trip</h4>
-        <v-layout justify-space-around="true">
-          <v-flex xs12 md6 class="row-input-margin">
-            <v-text-field v-model="trip.name" :counter="20" label="Trip Name" required></v-text-field>
-          </v-flex>
+<template xmlns:v-slot="http://www.w3.org/1999/XSL/Transform">
+    <div class="width-for-container">
+        <v-form ref="form" lazy-validation>
+            <v-container grid-list-xl>
+                <h4>Create a new trip</h4>
+                <v-layout justify-space-around="true">
+                    <v-flex xs12 md12 class="row-input-margin">
+                        <v-text-field v-model="trip.title" :counter="20" label="Trip Name" required></v-text-field>
+                    </v-flex>
+                </v-layout>
+                <ul>
+                    <li v-for="(destination, index) in trip.destinations" :v-bind="index" :key="index">
+                            <v-card class="destination-form-padding">
+                                    <v-layout>
+                                        <v-flex xs12 md4>
+                                            <v-combobox
+                                                    :rules="noSameDestinationNameConsecutiveRule"
+                                                    :items="destinations.map(dest => dest.name)"
+                                                    v-model="destination.title"
+                                                    label="Select an existing destination"
+                                            ></v-combobox>
+                                            <v-btn flat small color="error" v-on:click="deleteDestination(index)">Remove</v-btn>
+                                        </v-flex>
 
-          <v-flex xs12 md6>
-            <v-text-field :counter="20" label="Destination Type" required></v-text-field>
-          </v-flex>
-        </v-layout>
-        <ul>
-          <li v-for="(destination, index) in trip.destinations" :v-bind="index">
-            <v-card class="destination-form-padding">
-              <v-layout>
-                <v-flex xs12 md4>
-                  <v-combobox
-                    :items="destinationsNamesAndIndex"
-                    v-model="destination.title"
-                    label="Select an existing destination"
-                  ></v-combobox>
-                </v-flex>
-
-                <v-flex xs12 md4>
-                  <v-card class="times-padding">
-                    <!-- Arrival Time -->
-                    <v-menu
-                      ref="arrivalTimeMenu"
-                      v-model="arrivalTimeMenu"
-                      :close-on-content-click="false"
-                      :nudge-right="40"
-                      :return-value.sync="arrivalTime"
-                      lazy
-                      transition="scale-transition"
-                      offset-y
-                      full-width
-                      max-width="290px"
-                      min-width="290px"
-                    >
-                      <template v-slot:activator="{ on }">
-                        <v-text-field
-                          v-model="arrivalTime"
-                          label="Arrival time"
-                          prepend-icon="access_time"
-                          readonly
-                          v-on="on"
-                        ></v-text-field>
-                      </template>
-                      <v-time-picker
-                        v-if="arrivalTimeMenu"
-                        v-model="arrivalTime"
-                        full-width
-                        @click:minute="$refs.arrivalTimeMenu[index].save(arrivalTime)"
-                      ></v-time-picker>
-                    </v-menu>
-
-                    <!-- Arrival date -->
-                    <v-menu
-                      v-model="arrivalDateMenu"
-                      :close-on-content-click="false"
-                      :nudge-right="40"
-                      lazy
-                      transition="scale-transition"
-                      offset-y
-                      full-width
-                      min-width="290px"
-                    >
-                      <template v-slot:activator="{ on }">
-                        <v-text-field
-                          v-model="arrivalDate"
-                          label="Arrival date"
-                          prepend-icon="event"
-                          readonly
-                          v-on="on"
-                        ></v-text-field>
-                      </template>
-                      <v-date-picker v-model="arrivalDate" @input="arrivalDateMenu = false"></v-date-picker>
-                    </v-menu>
-                  </v-card>
-                </v-flex>
-                <v-flex xs12 md4>
-                  <v-card class="times-padding">
-                    <!-- Departure time -->
-                    <v-menu
-                      ref="departureTimeMenu"
-                      v-model="departureTimeMenu"
-                      :close-on-content-click="false"
-                      :nudge-right="40"
-                      :return-value.sync="departureTime"
-                      lazy
-                      transition="scale-transition"
-                      offset-y
-                      full-width
-                      max-width="290px"
-                      min-width="290px"
-                    >
-                      <template v-slot:activator="{ on }">
-                        <v-text-field
-                          v-model="departureTime"
-                          label="Departure time"
-                          prepend-icon="access_time"
-                          readonly
-                          v-on="on"
-                        ></v-text-field>
-                      </template>
-                      <v-time-picker
-                        v-if="departureTimeMenu"
-                        v-model="departureTime"
-                        full-width
-                        @click:minute="$refs.departureTimeMenu[index].save(departureTime)"
-                      ></v-time-picker>
-                    </v-menu>
-                    <!-- Departure date -->
-                    <v-menu
-                      v-model="departureDateMenu"
-                      :close-on-content-click="false"
-                      :nudge-right="40"
-                      lazy
-                      transition="scale-transition"
-                      offset-y
-                      full-width
-                      min-width="290px"
-                    >
-                      <template v-slot:activator="{ on }">
-                        <v-text-field
-                          v-model="departureDate"
-                          label="Departure date"
-                          prepend-icon="event"
-                          readonly
-                          v-on="on"
-                        ></v-text-field>
-                      </template>
-                      <v-date-picker v-model="departureDate" @input="departureDateMenu = false"></v-date-picker>
-                    </v-menu>
-                  </v-card>
-                </v-flex>
-              </v-layout>
-            </v-card>
-          </li>
-        </ul>
-
-        <v-layout>
-          <v-flex xs12 md6>
-            <v-text-field label="Latitude" required></v-text-field>
-          </v-flex>
-
-          <v-flex xs12 md6>
-            <v-text-field type="number" label="Longitude" required></v-text-field>
-          </v-flex>
-        </v-layout>
-        <div>
-          <v-btn color="red" v-on:click="resetValues">RESET VALUES</v-btn>
-          <v-btn>CREATE DESTINATION</v-btn>
-        </div>
-      </v-container>
-    </v-form>
-  </div>
+                                        <v-flex xs12 md4>
+                                            <v-card class="times-padding">
+                                                <!-- Arrival date -->
+                                                <v-menu
+                                                        v-model="destination.arrivalDateMenu"
+                                                        :close-on-content-click="false"
+                                                        :nudge-right="40"
+                                                        lazy
+                                                        transition="scale-transition"
+                                                        offset-y
+                                                        full-width
+                                                        min-width="290px"
+                                                >
+                                                    <template v-slot:activator="{ on }">
+                                                        <v-text-field
+                                                                v-model="destination.arrivalDate"
+                                                                label="Arrival date"
+                                                                prepend-icon="event"
+                                                                readonly
+                                                                v-on="on"
+                                                        ></v-text-field>
+                                                    </template>
+                                                    <v-date-picker v-model="destination.arrivalDate" @input="destination.arrivalDateMenu = false"></v-date-picker>
+                                                </v-menu>
+                                            </v-card>
+                                        </v-flex>
+                                        <v-flex xs12 md4>
+                                            <v-card class="times-padding">
+                                                <!-- Departure date -->
+                                                <v-menu
+                                                        v-model="destination.departureDateMenu"
+                                                        :close-on-content-click="false"
+                                                        :nudge-right="40"
+                                                        lazy
+                                                        transition="scale-transition"
+                                                        offset-y
+                                                        full-width
+                                                        min-width="290px"
+                                                >
+                                                    <template v-slot:activator="{ on }">
+                                                        <v-text-field
+                                                                v-model="destination.departureDate"
+                                                                label="Departure date"
+                                                                prepend-icon="event"
+                                                                readonly
+                                                                v-on="on"
+                                                        ></v-text-field>
+                                                    </template>
+                                                    <v-date-picker v-model="destination.departureDate" @input="destination.departureDateMenu = false"></v-date-picker>
+                                                </v-menu>
+                                            </v-card>
+                                    </v-flex>
+                                </v-layout>
+                            </v-card>
+                        </li>
+                </ul>
+                <div>
+                    <v-btn color="red" v-on:click="resetValues">CANCEL</v-btn>
+                    <v-btn v-on:click="createTrip">CREATE TRIP</v-btn>
+                    <v-btn v-on:click="addDestinationToTrip">ADD DESTINATION</v-btn>
+                </div>
+            </v-container>
+        </v-form>
+    </div>
 </template>
 
 <style>
-@import url("https://fonts.googleapis.com/css?family=Karla:400,700");
+    @import url("https://fonts.googleapis.com/css?family=Karla:400,700");
 
-.outer-container {
-  text-align: center;
-}
-
-.width-for-container {
-  width: 60%;
-}
-.destination-form-padding {
-  padding: 2em;
-}
-.times-padding {
-  padding: 1em;
-}
+    .width-for-container {
+        width: 60%;
+    }
+    .destination-form-padding {
+        padding: 2em;
+    }
+    .times-padding {
+        padding: 1em;
+    }
 </style>
 
 
 <script>
-// import { store } from "../../store/index";
-import moment from "moment";
-import { Datetime } from "vue-datetime";
-export default {
-  // store,
-  components: {
-    datetime: Datetime
-  },
-  props: {},
-  data() {
-    return {
-      arrivalTimeMenu: false,
-      arrivalDateMenu: false,
-      departureTimeMenu: false,
-      departureDateMenu: false,
-      arrivalDate: null,
-      departureDate: null,
-      arrivalTime: null,
-      departureTime: null,
-      trip: {
-        destinations: [
-          {
-            title: "Ciao",
-            arrivalTime: null,
-            departureTime: moment().add(1, "day")
-          }
-          // {
-          //   title: "Ciao",
-          //   arrivalTime: moment(),
-          //   departureTime: moment().add(1, "day")
-          // }
-        ]
-      }
+    import { store } from "../../store/index";
+    import { createTrip } from '../../repository/TripRepository';
+
+    export default {
+        store,
+        components: {
+        },
+        props: {},
+        data() {
+            return {
+                trip: {
+                    title: "",
+                    destinations: []
+                }
+            };
+        },
+        computed: {
+            // rules
+            noSameDestinationNameConsecutiveRule() {
+                let noConsecutiveSame = true;
+                const destinations = this.trip.destinations;
+                for (let i=0; i < destinations.length; i++) {
+                    if ((i + 1) < destinations.length) {
+                        if (destinations[i].title === destinations[i + 1].title) {
+                            noConsecutiveSame = false;
+                        }
+                    }
+                    if ((i - 1) >= 0) {
+                        if (destinations[i].title === destinations[i - 1].title) {
+                            noConsecutiveSame = false;
+                        }
+                    }
+                }
+                return [(noConsecutiveSame) || "Cannot have same destination consecutive"];
+            },
+            //
+            destinations() {
+                return store.state.destinations.destinations;
+            },
+        },
+        methods: {
+            resetValues: function() {
+                this.$refs.form.reset();
+            },
+            deleteDestination: function(index) {
+                let newDestinations = this.trip.destinations;
+                newDestinations.splice(index, 1);
+                this.trip.destinations = newDestinations;
+            },
+            addDestinationToTrip: function() {
+                console.log(this.trip.destinations);
+                const template = {
+                        title: null,
+                        arrivalDate: null,
+                        departureTime: null,
+                        departureDate: null,
+                        arrivalDateMenu: false,
+                        departureDateMenu: false,
+                };
+                let newDestinations = this.trip.destinations;
+                newDestinations.push(template);
+                this.trip.destinations = newDestinations;
+            },
+            createTrip: async function() {
+                if (this.$refs.form.validate()) {
+                    console.log('going throuhg');
+                    // {id: null, ordinal: null, arrivalTime: null, departureTime: null}
+                    let trip = {title: this.trip.title, destinations: [] };
+                    for (let destination of this.trip.destinations) {
+                        const destByIdId = this.destinations.find(dest => destination.title === dest.title);
+                        trip.destinations.push({id: destByIdId.id, arrivalDate: destination.arrivalDate, departureDate: destination.departureDate});
+                    }
+                    // TODO: wait for auth frontend
+                    const response = await createTrip(trip);
+                    console.log(response);
+                    // if (response.status) {
+                    //
+                    // }
+                }
+            }
+
+        }
     };
-  },
-  computed: {
-    destinations() {
-      return [
-        { title: "Ciao", something: "Something" },
-        { title: "bUONGIORNO", something: "Something" }
-      ];
-      // return store.state.destinations.destinations;
-    },
-    destinationsNamesAndIndex() {
-      // return store.state.destinations.destinations.map(
-      return this.destinations.map(destination => destination.title);
-    }
-  },
-  methods: {
-    resetValues: function() {
-      this.$refs.form.reset();
-    }
-  }
-};
 </script>
---
+++ frontend/travelea/src/repository/TripRepository.js
@@ -0,0 +1,18 @@
+export const createTrip = async (tripBody) => {
+
+    let url = "http://localhost:9000/trips";
+
+    const result = await fetch(url, {
+        method: "POST",
+        headers: {
+            // TODO: wait until authentication is in place
+            "Content-Type": "application/json",
+            "X-Authorization": "/////must retrieve this from the store/////"
+        },
+        body: JSON.stringify(tripBody),
+    }).then(response => {
+            return response;
+    });
+
+    return result;
+};
