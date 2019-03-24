0841a6745fc7530423bb4caaa093cafae649e335
Dana Lambert <dla72@uclive.ac.nz>
2019-03-24 15:56:46 +1300

    Adding a button to toggle uploading functionality and implementing the toggle function. Adjusting padding and trying to make the input element look slightly nicer.

--- frontend/travelea/src/components/profile/PersonalPhotos.vue
+++ frontend/travelea/src/components/profile/PersonalPhotos.vue
@@ -2,84 +2,79 @@
 
 <template>
   <div class="outer-container">
-    <div class="banner">
+  <!-- <v-container > -->
+     <div fluid class="banner">
+    <!-- <div class="banner"> -->
       <h1>MY PHOTOS</h1>
       <hr>
-    </div>
-    <div class="container">
-      <h2 class="headline mb-0">UPLOAD PHOTO</h2>
-      <v-divider></v-divider>
-      <div class="section">
-        <label>
-          <input type="file" id="file" ref="file" v-on:change="handleFileUpload()">
-        </label>
-        <v-btn v-on:click="submitFile()">Submit</v-btn>
-      </div>
+     </div>
+    
+    <!-- </div> -->
+    <!-- <div class="container"> -->
+     <v-container>
+      <!-- <h2 class="headline mb-0">UPLOAD PHOTO</h2>
+      <v-divider></v-divider>-->
 
-      <h2 class="headline mb-0">MY PHOTOS</h2>
-      <v-divider></v-divider>
+      <div class="section">
+        <h2 class='headline'>MY PHOTOS</h2>
+        <v-btn class="upload-toggle-button" fab small dark color="indigo"  @click="toggleShowUploadPhoto">
+          <v-icon dark>add</v-icon>
+        </v-btn>
+      </div>
+      <v-divider class='photo-header-divider'></v-divider>
+      <div  v-if="showUploadSection" >
+      <div class="upload-section  section">
+        <label>
+          <input class='choose-file-button' type="file" id="file" ref="file" v-on:change="handleFileUpload()">
+        </label>
+        <v-btn v-on:click="submitFile()">Upload Photo</v-btn>
+       </div>
+        <v-divider class='photo-header-divider'></v-divider>
+      </div>
+      
       <!-- <hr> -->
 
       <ul>
         <li v-for="row in files" :value="row.value" :key="row.value">
           <div class="personal-photo-row">
             <div v-for="item in row" :value="item.value" :key="item.value" class="image-container">
-              <v-img @click.stop="dialog = true" v-on:click="setDialogueContent(item)" class="personal-photo-element" :src="getImgUrl(item)"></v-img>
+              <v-img
+                @click.stop="dialog = true"
+                v-on:click="setDialogueContent(item)"
+                class="personal-photo-element"
+                :src="getImgUrl(item)"
+              ></v-img>
             </div>
           </div>
         </li>
       </ul>
-    </div>
+      <!-- </v-container> -->
+    <!-- </div> -->
+    
 
+    <v-dialog v-model="dialog" width="500">
+      <v-card>
+        <v-img :src="clickedImageURL" class="dialogue-image"></v-img>
 
-     <v-dialog
-                v-model="dialog"
-                width="500"
-              
-              >
+        <v-card-title primary-title>
+          <div>
+            <h5 class="headline mb-0">Image Name</h5>
+            <div>Description/Other meta info</div>
+          </div>
+        </v-card-title>
 
-                <v-card>
-                    <v-img
-                    :src="clickedImageURL"
-                    class="dialogue-image"
-                  ></v-img>
+        <v-divider></v-divider>
 
-                  <v-card-title primary-title>
-                    <div>
-                      <h5 class="headline mb-0">Image Name</h5>
-                      <div> Description/Other meta info </div>
-                    </div>
-                  </v-card-title>
-
-                  <v-divider></v-divider>
-
-                  <v-card-actions>
-                    <v-spacer></v-spacer>
-                      <v-switch
-                      v-model="publicPhotoSwitch"
-                      :label="`Public Photo`"
-                    ></v-switch>
-                       <v-btn
-                      color="primary"
-                      flat
-                      @click="updatePhotoVisability()"
-                    >
-                      Apply changes
-                    </v-btn>
-                    <v-btn
-                      color="primary"
-                      flat
-                      @click="dialog = false"
-                    >
-                      Close
-                    </v-btn>
-                  </v-card-actions>
-                </v-card>
-              </v-dialog>
-
-   
-    </div>
-
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-switch v-model="publicPhotoSwitch" :label="`Public Photo`"></v-switch>
+          <v-btn color="primary" flat @click="updatePhotoVisability()">Apply changes</v-btn>
+          <v-btn color="primary" flat @click="dialog = false">Close</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </v-container>
+  </div>
 </template>
 
 
@@ -91,6 +86,41 @@
 .dialogue-image {
   height: auto;
 }*/
+.choose-file-button {
+  background-color: #f5f5f5;
+  color: rgba(0,0,0,0.87);
+  font-size: 14px;
+  font-weight: 500;
+  cursor: pointer;
+
+  align-items: center;
+  border-radius: 2px;
+  display: inline-flex;
+  height: 36px;
+  flex: 0 0 auto;
+  font-size: 14px;
+  font-weight: 500;
+  justify-content: center;
+  margin: 6px 8px;
+  min-width: 88px;
+  outline: 0;
+  text-transform: uppercase;
+  text-decoration: none;
+  position: relative;
+  vertical-align: middle;
+}
+
+.upload-section {
+  padding: 20px 0px;
+}
+
+.upload-toggle-button {
+  align-self: flex-end;
+}
+
+.photo-header-divider {
+  margin-top:16px;
+}
 
 .image-container {
   width: 24%;
@@ -114,7 +144,7 @@
 
 .personal-photo-row {
   display: flex;
-  justify-content: space-between;
+  justify-content: space-evenly;
   margin-bottom: 30px;
 }
 
@@ -123,7 +153,7 @@ ul {
 }
 
 h2 {
-  padding-bottom: 10px;
+align-self: flex-end;
 }
 
 hr {
@@ -200,7 +230,8 @@ export default {
       clickedImageURL: "",
       clickedImage: {},
       dialog: false,
-      publicPhotoSwitch: false
+      publicPhotoSwitch: false,
+      showUploadSection: false
     };
   },
 
@@ -210,17 +241,19 @@ export default {
   //   }
   // },
 
-  
-
   methods: {
     // Sets the file property the the file being uploaded.
     handleFileUpload() {
       this.file = this.$refs.file.files[0];
     },
 
+    toggleShowUploadPhoto: function() {
+      this.showUploadSection = !this.showUploadSection;
+    },
+
     updatePhotoVisability() {
       this.clickedImage.is_public = this.publicPhotoSwitch;
-      updatePersonalPhoto(this.clickedImage)
+      updatePersonalPhoto(this.clickedImage);
     },
 
     // Submits the image file and uploads it to the server
@@ -236,16 +269,16 @@ export default {
     },
 
     // Gets the local image file path
-    getImgUrl(item, place="somewhere else") {
+    getImgUrl(item, place = "somewhere else") {
       return require("../../../../../backend/resources/images/" +
         item.photo_filename);
     },
 
     // Gets the local image file path
-    setDialogueContent(selectedImage="") {
+    setDialogueContent(selectedImage = "") {
       this.dialog = true;
       this.clickedImage = selectedImage;
-      this.publicPhotoSwitch = selectedImage.is_public
+      this.publicPhotoSwitch = selectedImage.is_public;
       this.clickedImageURL = this.getImgUrl(selectedImage);
     },
 
