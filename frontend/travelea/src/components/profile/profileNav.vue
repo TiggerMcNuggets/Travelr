6839d1eaf7d5e8da4699a0307ebd78dc5e2b2f4e
cwenborn <cwe55@uclive.ac.nz>
2019-03-26 12:03:23 +1300

    Changed profile page to get the stored id from store so when getting/setting profile it has an id - changes are currently commented out until login has been fixed

--- frontend/travelea/src/components/profile/profileNav.vue
+++ frontend/travelea/src/components/profile/profileNav.vue
@@ -8,8 +8,8 @@
                     <img src="https://specials-images.forbesimg.com/imageserve/5b153a1031358e612fbb0afb/416x416.jpg?background=000000&cropX1=642&cropX2=2255&cropY1=29&cropY2=1641">
                 </v-avatar>
             </v-layout>
-            <v-flex xs10>{{ profile.profile.firstName}} {{ profile.profile.firstName}}      </v-flex> 
-            <v-flex xs10>DOB: {{ profile.profile.dateOfBirth }}       </v-flex>         
+            <v-flex xs10>{{ profile.firstName}} {{ profile.firstName}}      </v-flex> 
+            <v-flex xs10>DOB: {{ profile.dateOfBirth }}       </v-flex>         
         </v-layout>
     </v-container>
 </template>
@@ -26,16 +26,22 @@
 <script>
 
 import {RepositoryFactory} from "../../repository/RepositoryFactory"
-let profileRepository = RepositoryFactory.get("profile")
+let profileRepository = RepositoryFactory.get("profile");
+import { store } from "../../store/index";
+
 
 export default {
+    store,
     data () {
         return {   
             profile:{}  
     }
     },
-    created: function() {  
-        this.profile = profileRepository.getProfile()
+    created: async function() {  
+//        let user_id = store.state.user.getId;
+//        console.log(user_id);
+//        this.profile = await profileRepository.getProfile(user_id);
+        this.profile = await profileRepository.getProfile();
         console.log(this.profile);    
     }
 }
