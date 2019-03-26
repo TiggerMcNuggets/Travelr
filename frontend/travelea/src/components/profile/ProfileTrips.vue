865f3048a3a7daa7677985a4352e43e0f5771d77
Dana Lambert <dla72@uclive.ac.nz>
2019-03-26 19:50:44 +1300

    Adding redirects and back buttons for both destinations and trips on profile.

--- frontend/travelea/src/components/profile/ProfileTrips.vue
+++ frontend/travelea/src/components/profile/ProfileTrips.vue
@@ -1,38 +1,43 @@
 /* eslint-disable */
 
 <template>
-   <div class="profile-outer-container">
-       <div class='profile-inner-container'>
-        <v-layout row>
-            <aside>
-                
-                <ProfileNav 
-                    :fname.sync="traveller.firstName" 
-                    :mname.sync="traveller.middleName" 
-                    :lname.sync="traveller.lastName" 
-                    :dob.sync="dateOfBirth" 
-                    :gender.sync="traveller.gender" 
-                    :types.sync="traveller.travellerTypes" 
-                    :nationalities.sync="nationalities" 
-                    :passports.sync="passports" />
-            </aside>
+  <div class="profile-outer-container">
+    <div class="profile-inner-container">
+      <v-layout row>
+        <aside>
+          <ProfileNav
+            :fname.sync="traveller.firstName"
+            :mname.sync="traveller.middleName"
+            :lname.sync="traveller.lastName"
+            :dob.sync="dateOfBirth"
+            :gender.sync="traveller.gender"
+            :types.sync="traveller.travellerTypes"
+            :nationalities.sync="nationalities"
+            :passports.sync="passports"
+          />
+        </aside>
 
-            <main class='profile-main'>
-                <v-card>
-                  <trips>
-        </trips>
-                </v-card>   
-                   
-            </main>
-
-          
-        </v-layout> 
-  
-
-       
-         </div>
+        <main class="profile-main">
+           
+          <v-card>
+            <v-btn
+              class="upload-toggle-button"
+              fab
+              small
+              dark
+              color="indigo"
+              @click="$router.go(-1)"
+            >
+         
+              <v-icon dark>keyboard_arrow_left</v-icon>
+            </v-btn>
+              
+            <trips></trips>
+          </v-card>
+        </main>
+      </v-layout>
     </div>
-   
+  </div>
 </template>
 
   <!-- <v-flex xs9 ml-5>
@@ -44,34 +49,32 @@
             </v-flex> -->
 
 <style>
-
 .trips-tile {
-    margin-left: 20px;
-  
+  margin-left: 20px;
 }
 
 .destinations-tile {
-    margin-top: 20px;
-    height: 290px;
+  margin-top: 20px;
+  height: 290px;
 }
 
 .photos-tile {
-    height: 300px;
+  height: 300px;
 }
 
 aside {
-    width: 25%;
-    margin-right: 20px 
+  width: 25%;
+  margin-right: 20px;
 }
 
 main {
-    margin-left: 20px;
+  margin-left: 20px;
 }
 
 .profile-inner-container {
-    width: 100%;
-     margin: 20px;
-    /* align-self: center; */
+  width: 100%;
+  margin: 20px;
+  /* align-self: center; */
 }
 
 .profile-outer-container {
@@ -81,25 +84,23 @@ main {
 }
 
 .profile-main {
-    width: 100%;
-    margin: 0px;
+  width: 100%;
+  margin: 0px;
+  text-align: left;
 }
-
 </style>
 
 
 <script>
-
 // import {RepositoryFactory} from "../../repository/RepositoryFactory"
-import ProfileNav from "./profileNav"
-import Trips from  "../trips/Trips"
-import PersonalPhotos from  "./PersonalPhotos2"
+import ProfileNav from "./profileNav";
+import Trips from "../trips/Trips";
+import PersonalPhotos from "./PersonalPhotos2";
 import dateTime from "../common/dateTime/dateTime.js";
 import ProfileRepository from "../../repository/ProfileRepository";
 
-import {store} from "../../store/index";
+import { store } from "../../store/index";
 // let profileRepository = RepositoryFactory.get("profile")
-
 
 export default {
   name: "Profile",
@@ -111,16 +112,16 @@ export default {
 
       dateOfBirth: "",
       nationalities: [],
-      passports: [],
+      passports: []
     };
   },
 
-   components: {
-        ProfileNav,
-        Trips,
-        PersonalPhotos
-    },
-  
+  components: {
+    ProfileNav,
+    Trips,
+    PersonalPhotos
+  },
+
   mounted() {
     this.getTraveller();
   },
@@ -133,12 +134,19 @@ export default {
     },
 
     setTravellerToFields() {
-      [this.nationalities, this.passports] = travellerFormHelper.convertFromNationalitiesRes(this.traveller.nationalities);
-      this.traveller.travellerTypes = travellerFormHelper.convertFromTravellerTypesRes(this.traveller.travellerTypes)
-      this.dateOfBirth = dateTime.convertTimestampToString(this.traveller.dateOfBirth);
-      
+      [
+        this.nationalities,
+        this.passports
+      ] = travellerFormHelper.convertFromNationalitiesRes(
+        this.traveller.nationalities
+      );
+      this.traveller.travellerTypes = travellerFormHelper.convertFromTravellerTypesRes(
+        this.traveller.travellerTypes
+      );
+      this.dateOfBirth = dateTime.convertTimestampToString(
+        this.traveller.dateOfBirth
+      );
     }
-
   }
 };
 </script>
