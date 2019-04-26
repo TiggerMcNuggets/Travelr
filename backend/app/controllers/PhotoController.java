07a390e95e41593f16c13c969391c0ca5ecf4528
Lorenzo <faso.9603@gmail.com>
2019-04-26 15:37:10 +1200

    Added isAdmin flag

--- backend/app/controllers/PhotoController.java
+++ backend/app/controllers/PhotoController.java
@@ -51,8 +51,9 @@ public class PhotoController extends Controller {
         fh.make_directory("resources/images");
 
         User user = request.attrs().get(Attrs.USER);
+        Boolean isAdmin = request.attrs().get(Attrs.IS_USER_ADMIN)
 
-        return personalPhotoRepository.list(id, user.id == id || user.accountType > 0).thenApplyAsync((photos) -> {
+        return personalPhotoRepository.list(id, user.id == id || isAdmin).thenApplyAsync((photos) -> {
             PathProperties pathProperties = PathProperties.parse("id,photo_filename,is_public");
             return ok(Ebean.json().toJson(photos, pathProperties));
         });
