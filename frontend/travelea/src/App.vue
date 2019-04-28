e147218898b5a95dce0bf48b93a44c97dfbc21c6
Adam Conway <aco108@uclive.ac.nz>
2019-04-29 01:43:30 +1200

    Restructured destination page and nav bar #implement #story[327]

--- frontend/travelea/src/App.vue
+++ frontend/travelea/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-app>
+  <v-app light>
     <v-navigation-drawer app temporary clipped v-model="drawer">
       <v-list>
         <v-list-tile v-for="item in menuOptions" :key="item.name" :to="item.link">
@@ -14,21 +14,34 @@
           </v-list-tile-action>
           <v-list-tile-content>Logout</v-list-tile-content>
         </v-list-tile>
-
       </v-list>
     </v-navigation-drawer>
-    <v-toolbar color="indigo" dark dense clipped-left fixed app>
+    <v-toolbar class="nav-bar" flat color="white" dense clipped-left fixed app>
       <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
-      <router-link to="/" class="white--text"><v-toolbar-title>Travelr</v-toolbar-title></router-link>
+      <router-link to="/" ><v-toolbar-title>Travelr</v-toolbar-title></router-link>
+      <v-spacer> </v-spacer>
+      <v-btn pr-4>Sign Up</v-btn>
+      <v-btn pl-4>Login </v-btn>
     </v-toolbar>
-    <v-content>
-      <v-container fluid pa-0 ma-0>
+    <v-content class="main-container">
+      <v-container fluid pa-0 ma-0 >
         <router-view></router-view>
       </v-container>
     </v-content>
   </v-app>
 </template>
 
+<style>
+
+.main-container {
+  margin-left: 0px;
+}
+
+.nav-bar {
+  border-bottom: 1px solid #edeff1 !important;
+}
+
+</style>
 <script>
 
 import {store} from "./store/index";
--- frontend/travelea/src/components/destination/DestinationListPage.vue
+++ frontend/travelea/src/components/destination/DestinationListPage.vue
@@ -1,18 +1,59 @@
 <template>
-<v-layout
-    <v-flex xs10>
-    <v-layout column align-center justify-start="">
-        <v-flex blue>
-            test
+  <v-layout>
+    <v-flex xs12>
+      <v-layout wrap class="outer-container" justify-center fill-height>
+        <v-flex xs12 class="secondary-nav-container">
+          <v-layout justify-center>
+            <v-flex md8 sm12 class="secondary-nav-bar" >
+              Secondary Navbar
+            </v-flex>   
+          </v-layout>       
         </v-flex>
-        <v-flex blue>
-            test
+        <v-flex md8 sm12 class="inner-container" pt-3>
+          <v-layout row wrap justify-center align-center>
+            <v-flex v-for="index in 10" :key="index" pb-2 px-1>
+              <v-card width=200px>
+                <v-img                               
+                  src="https://www.canterbury.ac.nz/homepage-events-carousel/matari-building-home-events.jpg">
+                </v-img>
+                <v-card-text pa-0>
+                UC | Christchurch | New Zealand
+                </v-card-text>
+              </v-card>
+            </v-flex>
+          </v-layout>         
         </v-flex>
-
-    </v-layout>
+      </v-layout>
+    </v-flex>
+  </v-layout>
 </template>
 
 <style>
+
+
+.outer-container {
+  /* border: 1px solid pink; */
+}
+
+.inner-container {
+  /* border: 1px solid blue; */
+}
+
+.secondary-nav-bar {
+    /* border: 2px solid orange; */
+    background-color: white;
+    height: 40px;
+}
+
+.secondary-nav-container {
+  background-color: white;
+}
+
+.main-content {
+    /* border: 2px solid green; */
+}
+
+
 
 </style>
 
@@ -27,7 +68,9 @@ export default {
   store,
   // local variables
   data() {
-    return {      
+    return { 
+      destinations: [
+      ]     
     };
   }, 
    // child components
@@ -36,6 +79,17 @@ export default {
 //   },
   methods: {
     
+  },
+  computed: {
+    containerWidth () {
+       switch (this.$vuetify.breakpoint.name) {
+        case 'xs': return '220px'
+        case 'sm': return '400px'
+        case 'md': return '960px'
+        case 'lg': return '1200px'
+        case 'xl': return '1200px'
+      }
+    }
   },
   created: function() {    
   }  
