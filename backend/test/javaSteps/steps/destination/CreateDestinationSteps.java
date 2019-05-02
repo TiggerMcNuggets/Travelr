97efa66d6e90a152baf5b3927e9e873040e107e4
Marvin <mgo81@uclive.ac.nz>
2019-05-02 14:34:45 +1200

    fixed broken tests

--- backend/test/javaSteps/steps/destination/CreateDestinationSteps.java
+++ backend/test/javaSteps/steps/destination/CreateDestinationSteps.java
@@ -50,12 +50,12 @@ public class CreateDestinationSteps {
     public void i_create_a_destination_for_user_with_id(int int1) {
         try {
             // Create request object
-            System.out.println("http://localhost:9000/api/users/"+int1+"/destinations");
+            System.out.println("http://localhost:9000/api/user/"+int1+"/destinations");
             Http.RequestBuilder createDestinationForUser = Helpers.fakeRequest()
                     .method("POST")
                     .header("X-Authorization", state.getToken())
                     .bodyJson(destinationData)
-                    .uri("http://localhost:9000/api/users/"+ int1 +"/destinations");
+                    .uri("http://localhost:9000/api/users/"+int1+"/destinations");
 
             // Send request
             state.setResult(route(state.getApplication(), createDestinationForUser));
