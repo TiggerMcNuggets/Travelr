8d5a395dbd249c2648abb48e4d1505cea4429686
Dana Lambert <dla72@uclive.ac.nz>
2019-05-06 10:40:04 +1200

    Adding photo multiple selection component and adding example usage to profile page. #story[328]

---
+++ frontend/travelea/src/components/photos/PhotoSelect.vue
@@ -0,0 +1,208 @@
+/* eslint-disable */
+
+<template>
+  <v-card>
+    <div class="outer-container">
+      <div class="inner-container">
+        <v-card-title primary-title>
+          <h2 class="headline">Choose Existing Photo</h2>
+        </v-card-title>
+
+        <v-divider></v-divider>
+
+        <ul>
+          <li v-for="row in files" :value="row.value" :key="row.value">
+            <div class="personal-photo-row">
+              <div
+                v-for="item in row"
+                :value="item.value"
+                :key="item.value"
+                class="image-container"
+              >
+                <v-icon v-if="imageIsSelected(item)" class="lock-icon" left>done</v-icon>
+                <div v-if="imageIsSelected(item)" class="triangle image-selected"></div>
+
+                <v-img
+                  v-on:click="selectImage(item)"
+                  class="personal-photo-element"
+                  :src="getImgUrl(item)"
+                ></v-img>
+              </div>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <v-divider></v-divider>
+    <v-btn color="primary" flat @click="closeDialogue">Close</v-btn>
+    <v-btn color="primary" flat v-on:click="setDestinationImages()">Add Photos to Destination</v-btn>
+  </v-card>
+</template>
+
+
+<style>
+.image-selected {
+  width: 0;
+  height: 0;
+  border-style: solid;
+  border-width: 100px 100px 0 0;
+  border-color: greenyellow transparent transparent transparent !important;
+  opacity: 0.3;
+  position: absolute;
+  z-index: 10;
+}
+
+.lock-icon {
+  color: white !important;
+  opacity: 1;
+  position: absolute;
+  z-index: 12;
+  font-size: 2.3em;
+  margin-top: 0.3em;
+  margin-left: 0.3em;
+  align-self: flex-start !important;
+}
+
+.photo-header-divider {
+  margin-top: 16px;
+}
+
+.image-container {
+  width: 24%;
+  height: 270px;
+  border: 1px solid lightgrey;
+  background-position: center;
+  padding: 7px;
+  overflow: hidden;
+  display: flex;
+  justify-content: flex-start;
+}
+
+.image-container:hover .personal-photo-element {
+  cursor: pointer;
+  opacity: 0.8;
+}
+
+.personal-photo-element {
+  height: 100%;
+  overflow: hidden;
+}
+
+.personal-photo-row {
+  display: flex;
+  justify-content: space-evenly;
+  margin-bottom: 30px;
+}
+
+ul {
+  padding-left: 0px;
+}
+
+h2 {
+  align-self: flex-end;
+}
+
+hr {
+  margin-bottom: 25px;
+}
+
+.section {
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+}
+
+</style>
+
+
+<script>
+import { store } from "../../store/index";
+import base_url from "../../repository/BaseUrl";
+import {
+  getImages
+} from "../../repository/PersonalPhotosRepository";
+
+export default {
+  store,
+
+  // local variables
+  data() {
+    return {
+      files: [],
+      clickedImageURL: "",
+      id: null,
+      selectedImages: []
+    };
+  },
+
+  props: ["closeDialogue"],
+
+  methods: {
+
+    //sets the user's profile photo as the selected
+    setDestinationImages() {
+      // This might be a call back from props, but this is where the stuff happens with the selected images/image.
+      console.log(this.selectedImages);
+      this.closeDialogue();
+    },
+
+    // Gets the image from the server
+    getImgUrl(item) {
+      return base_url + "/api/travellers/photo/" + item.photo_filename;
+    },
+
+    // Checks if the photo is selected that is it is present in the selected images list.
+    imageIsSelected(selectedImage) {
+      for (let i = 0; i <= this.selectedImages.length; i++) {
+        if (selectedImage === this.selectedImages[i]) {
+          return true;
+        }
+      }
+      return false;
+    },
+
+    // Unselects the image and removes from the list of selected images.
+    unselectImage(selectedImage) {
+      for (let i = 0; i <= this.selectedImages.length; i++) {
+        if (selectedImage === this.selectedImages[i]) {
+          this.selectedImages.splice(i, 1);
+        }
+      }
+    },
+
+    // Toggles if the image is selected or not adding or removing from selected list.
+    selectImage(selectedImage) {
+      this.imageIsSelected(selectedImage)
+        ? this.unselectImage(selectedImage)
+        : this.selectedImages.push(selectedImage);
+    },
+
+    // Groups the images into rows
+    groupImages(imageList) {
+      let newImageList = [];
+      let row = [];
+      const num_cols = 4;
+      for (let i = 0; i < imageList.length; i++) {
+        if (i % num_cols === 0 && row.length !== 0) {
+          newImageList.unshift(row);
+          row = [];
+        }
+        row.push(imageList[i]);
+      }
+
+      if (row) newImageList.unshift(row);
+      newImageList.reverse();
+      return newImageList;
+    }
+  },
+
+  // Initialises the component with the image data.
+  created: function() {
+    this.id = this.$route.params.id;
+
+    getImages(this.id).then(result => {
+      this.files = this.groupImages(result.data);
+    });
+  }
+};
+</script>
