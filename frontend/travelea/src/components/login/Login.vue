a9b82a90687a5c83c41e3fb493e0e0152eccc0b2
Dana Lambert <dla72@uclive.ac.nz>
2019-05-08 13:07:38 +1200

    Documenting views for miscelanious compoents. #story[313]

--- frontend/travelea/src/components/login/Login.vue
+++ frontend/travelea/src/components/login/Login.vue
@@ -1,89 +1,79 @@
 <template>
-    <div class="width-for-container">
-        <div class="form-container">
-        
-            <h1>Login</h1>
-    
-            <v-text-field
-                    v-model="user.email"
-                    label="Email"
-            ></v-text-field>
+  <div class="width-for-container">
+    <div class="form-container">
+      <h1>Login</h1>
 
-            <v-text-field
-                    v-model="user.password"
-                    label="Password"
-                    type="password"
-            ></v-text-field>
+      <v-text-field v-model="user.email" label="Email"></v-text-field>
 
-            <v-btn class="login-button" large round v-on:click="login" color="primary">Login</v-btn>
-            <p>Don't have an account? <router-link to="/signup">Click here</router-link> to sign up</p>
-            <v-alert :value="loginAlert" color="error">Incorrect email and/or password</v-alert>
-        </div>
-     </div>
+      <v-text-field v-model="user.password" label="Password" type="password"></v-text-field>
+
+      <v-btn class="login-button" large round v-on:click="login" color="primary">Login</v-btn>
+      <p>
+        Don't have an account?
+        <router-link to="/signup">Click here</router-link>to sign up
+      </p>
+      <v-alert :value="loginAlert" color="error">Incorrect email and/or password</v-alert>
+    </div>
+  </div>
 </template>
 
 <style>
 @import url("https://fonts.googleapis.com/css?family=Karla:400,700");
 
-.outer-container {
-  text-align: center;
-}
-
-.form-element {
-    background-color: darkred;
-}
-
 .login-button {
-    margin: 20px 0px;
+  margin: 20px 0px;
 }
 
 .form-container {
-    width: 50%;
-    margin: 50px 0px;
-    padding: 20px;
+  width: 50%;
+  margin: 50px 0px;
+  padding: 20px;
 }
 
 h1 {
-    margin: 50px 0px;
-    font-size: 42px;
+  margin: 50px 0px;
+  font-size: 42px;
 }
 
 .width-for-container {
-    width: 100%;
-    height: 100%;
-    display: flex;
-    justify-content: center;
-    vertical-align: middle;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  vertical-align: middle;
 }
 </style>
 
 <script>
-
-
 import { store } from "../../store/index";
 
-
 export default {
-    store,
-    data() {
-        return {
-          user: {},
-          loggedIn: false,
-          loginAlert: false,
-        };
-      },
-    methods: {
-        login() {
-            this.loginAlert = false;
-            store.dispatch("login", this.user)
-            .then(() => {
-                this.$router.push("/");
-            })
-            .catch(() => {
-                console.log("invalid login")
-                this.loginAlert = true;
-            })
-        }
+  store,
+  data() {
+    return {
+      user: {},
+      loggedIn: false,
+      loginAlert: false
+    };
+  },
+  methods: {
+    /** Allows the user to login. This calls a function which validates the user data and if valid sets it as the
+     * signed in user within the store as well as the returned auth token. This is used for user validation within the
+     * app. Redirects the user to their profile page on success.
+     * If the user has incorrect credentials an error alert is displayed.
+     */
+    login() {
+      this.loginAlert = false;
+      store
+        .dispatch("login", this.user)
+        .then(() => {
+          this.$router.push("/");
+        })
+        .catch(() => {
+          console.log("invalid login");
+          this.loginAlert = true;
+        });
     }
-}
+  }
+};
 </script>
--- frontend/travelea/src/components/logout/Logout.vue
+++ frontend/travelea/src/components/logout/Logout.vue
@@ -1,13 +1,12 @@
 <template>
-<button></button>
+  <button></button>
 </template>
 
 <style>
-
 </style>
 
 <script>
-import {store} from "../../store/index";
+import { store } from "../../store/index";
 
 export default {
   name: "Logout",
@@ -19,16 +18,20 @@ export default {
     this.logout();
   },
   methods: {
+    /**
+     * Logs the user out and sets their auth token to null in the store. Redirects the user back to login.
+     */
     logout() {
-      store.dispatch("logout").then(() => {
-        localStorage.setItem("token", "");
-        this.$router.push("/login");
-      })
-      .catch(() => {
-        localStorage.setItem("token", "");
-        this.$router.push("/login");
-      });
-
+      store
+        .dispatch("logout")
+        .then(() => {
+          localStorage.setItem("token", "");
+          this.$router.push("/login");
+        })
+        .catch(() => {
+          localStorage.setItem("token", "");
+          this.$router.push("/login");
+        });
     }
   }
 };
