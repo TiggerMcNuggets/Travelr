ba724ac88b9b6710a8ff7065ea39bd05ffd05605
Lorenzo Fasano <fasano.lorenzo96@gmail.com>
2019-05-22 16:35:06 +1200

    Added Nationality is_old flag #story[400]

--- backend/app/models/Nationality.java
+++ backend/app/models/Nationality.java
@@ -4,16 +4,22 @@ package models;
 import io.ebean.Finder;
 import play.data.validation.Constraints;
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 
 @Entity
 public class Nationality extends BaseModel {
 
     public static final Finder<Long, Nationality> find = new Finder<>(Nationality.class);
 
+    @NotNull
+    @Column(columnDefinition = "boolean default 0")
+    private boolean is_old;
+
     @Constraints.Required
     public String name;
 
     public Nationality(String name) {
         this.name = name;
+        this.is_old = false;
     }
 }
