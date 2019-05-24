39b044238868f01015fa1a2113602cacae679493
Dana Lambert <dla72@uclive.ac.nz>
2019-05-24 14:21:59 +1200

    Added javadoc to admin constants.

--- backend/app/controllers/constants/AdminConstants.java
+++ backend/app/controllers/constants/AdminConstants.java
@@ -1,7 +1,13 @@
 package controllers.constants;
 
+/**
+ * Constants relating to admin.
+ */
 public class AdminConstants {
 
+    /**
+     * The fixed id of the global admin.
+     */
     public static final Long ADMIN_ID = Long.valueOf(1);
 
 }
