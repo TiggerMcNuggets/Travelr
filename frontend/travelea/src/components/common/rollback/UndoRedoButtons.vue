80d7f138cd2d968d679a69c4ce82d3952998bf3a
Dana Lambert <dla72@uclive.ac.nz>
2019-05-24 17:30:23 +1200

    Adding key events to undo and redo. #story[399]

--- frontend/travelea/src/components/common/rollback/UndoRedoButtons.vue
+++ frontend/travelea/src/components/common/rollback/UndoRedoButtons.vue
@@ -1,35 +1,49 @@
 <template>
-    <div>
-        <v-btn
-                class="upload-toggle-button"
-                fab
-                small
-                flat
-                :disabled="!canUndo"
-                @click="undo"
-        >
-            <v-icon dark>undo</v-icon>
-        </v-btn>
-        <v-btn
-                class="upload-toggle-button"
-                fab
-                small
-                flat
-                :disabled="!canRedo"
-                @click="redo"
-        >
-            <v-icon dark>redo</v-icon>
-        </v-btn>
-    </div>
+  <div>
+    <v-btn
+      class="upload-toggle-button"
+      fab
+      small
+      flat
+      :disabled="!canUndo"
+      @click="undo"
+      @keyup.enter="undo"
+    >
+      <v-icon dark>undo</v-icon>
+    </v-btn>
+    <v-btn class="upload-toggle-button" fab small flat :disabled="!canRedo" @click="redo">
+      <v-icon dark>redo</v-icon>
+    </v-btn>
+  </div>
 </template>
 
 <script>
-    export default {
-        props: {
-            canUndo: Boolean,
-            canRedo: Boolean,
-            undo: Function,
-            redo: Function
+export default {
+  props: {
+    canUndo: Boolean,
+    canRedo: Boolean,
+    undo: Function,
+    redo: Function
+  },
+
+  mounted() {
+    window.addEventListener(
+      "keyup",
+      function(event) {
+        if (event.keyCode == 90 && event.ctrlKey) {
+          this.undo();
         }
-    }
+      }.bind(this)
+    );
+
+    window.addEventListener(
+      "keyup",
+      function(event) {
+        if (event.keyCode == 89 && event.ctrlKey) {
+          this.redo();
+        }
+      }.bind(this)
+    );
+  }
+};
 </script>
