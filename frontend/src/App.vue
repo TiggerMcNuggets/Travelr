dea833ebc9a03180a9604a17fa58ed4a747a37a4
Dana Lambert <dla72@uclive.ac.nz>
2019-07-08 23:36:48 +1200

    Changing the app layout to include a collapsable side nav and resizable content. #story490

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,52 +1,89 @@
 <template>
-  <v-app>
-    <v-navigation-drawer temporary clipped app v-model="drawer">
-      <v-list>
-        <v-list-tile v-for="item in menuOptions" :key="item.name" :to="item.link">
-          <v-list-tile-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>{{ item.name }}</v-list-tile-content>
-        </v-list-tile>
-        <v-list-tile v-if="loggedIn" v-on:click="logout">
-          <v-list-tile-action>
-            <v-icon>lock_open</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>Logout</v-list-tile-content>
-        </v-list-tile>
+  <div id="app">
+    <v-app id="inspire">
+      <v-toolbar fixed app clipped-right class="main-header">
+        <router-link to="/" class="primary-logo">
+          <v-toolbar-title class="fill-height">
+            <img class="fill-height" src="./assets/logo_white.png" />
+          </v-toolbar-title>
+        </router-link>
+      </v-toolbar>
+      <v-navigation-drawer fixed v-model="drawer" app :mini-variant.sync="mini" hide-overlay>
+        <v-toolbar flat class="transparent">
+          <v-list class="pa-0">
+            <v-list-tile avatar>
+              <v-list-tile-avatar>
+                <img src="https://randomuser.me/api/portraits/men/85.jpg" />
+              </v-list-tile-avatar>
 
-      </v-list>
-    </v-navigation-drawer>
-    <v-toolbar color="indigo" dark dense clipped-left fixed app>
-      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
-      <router-link to="/" class="white--text"><v-toolbar-title>Travelr</v-toolbar-title></router-link>
-    </v-toolbar>
-    <v-content class="main-content">
-      <v-container class="main-container" fluid pa-0 ma-0>
-        <router-view></router-view>
-      </v-container>
-    </v-content>
-  </v-app>
+              <v-list-tile-content>
+                <v-list-tile-title>John Leider</v-list-tile-title>
+              </v-list-tile-content>
+
+              <v-list-tile-action>
+                <v-btn icon @click.stop="mini = !mini">
+                  <v-icon>chevron_left</v-icon>
+                </v-btn>
+              </v-list-tile-action>
+            </v-list-tile>
+          </v-list>
+        </v-toolbar>
+
+        <v-list class="pt-0">
+          <v-divider></v-divider>
+          <v-list-tile v-for="item in menuOptions" :key="item.name" :to="item.link">
+            <v-list-tile-action>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>{{ item.name }}</v-list-tile-content>
+          </v-list-tile>
+          <v-list-tile v-if="loggedIn" v-on:click="logout">
+            <v-list-tile-action>
+              <v-icon>lock_open</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>Logout</v-list-tile-content>
+          </v-list-tile>
+        </v-list>
+      </v-navigation-drawer>
+      <v-content>
+        <v-container fluid fill-height ma-0 pa-0>
+          <v-layout>
+            <v-flex>
+              <router-view></router-view>
+            </v-flex>
+          </v-layout>
+        </v-container>
+      </v-content>
+    </v-app>
+  </div>
 </template>
 
-<style>
-.main-content {
-  padding-left: 0 !important;
-  margin-left: 0 !important;
+
+
+<style lang="scss">
+@import "./assets/css/main.scss";
+
+.primary-logo {
+  height: 50%;
 }
 
+.fill-height {
+  height: 100%;
+}
 </style>
 
-<script>
 
-import {store} from "./store/index";
+<script>
+import { store } from "./store/index";
 
 export default {
   name: "App",
   store,
   data() {
     return {
-      drawer: false
+      drawer: true,
+      mini: true,
+      right: null
     };
   },
   computed: {
@@ -58,35 +95,55 @@ export default {
 
       if (store.getters.isLoggedIn) {
         menuOptions = [
-          { name: "Profile", icon: "account_circle", link: "/user/"+store.getters.getUser.id },
+          {
+            name: "Profile",
+            icon: "account_circle",
+            link: "/user/" + store.getters.getUser.id
+          },
           { name: "Users", icon: "supervised_user_circle", link: "/users" },
           { name: "Destination Map", icon: "map", link: "/destinations" },
-          { name: "Destination List", icon: "list", link: "/user/"+store.getters.getUser.id+"/destinations" },
-          { name: "My Trips", icon: "flight", link: "/user/"+store.getters.getUser.id+"/trips" },
+          {
+            name: "Destination List",
+            icon: "list",
+            link: "/user/" + store.getters.getUser.id + "/destinations"
+          },
+          {
+            name: "My Trips",
+            icon: "flight",
+            link: "/user/" + store.getters.getUser.id + "/trips"
+          }
         ];
       }
       if (store.getters.getIsUserAdmin && store.getters.isLoggedIn) {
-        menuOptions.push({ name: "Admin Panel", icon: "playlist_add_check", link: "/admin_dash" });
-        menuOptions.push({ name: "Edit Requests", icon: "done_all", link: "/edit_requests" });
+        menuOptions.push({
+          name: "Admin Panel",
+          icon: "playlist_add_check",
+          link: "/admin_dash"
+        });
+        menuOptions.push({
+          name: "Edit Requests",
+          icon: "done_all",
+          link: "/edit_requests"
+        });
       }
 
       return menuOptions;
     },
 
     loggedIn() {
-      return store.getters.isLoggedIn
+      return store.getters.isLoggedIn;
     }
   },
   methods: {
     logout() {
-      store.dispatch("logout")
-      .then(() => {
-        
-        this.$router.push("/login")
-      })
-      .catch(() => {
-        this.$router.push("/login")
-      })
+      store
+        .dispatch("logout")
+        .then(() => {
+          this.$router.push("/login");
+        })
+        .catch(() => {
+          this.$router.push("/login");
+        });
     }
   }
 };
