cb8fd0c4579d0869684ce0f411abaf60d67c7b75
Dana Lambert <dla72@uclive.ac.nz>
2019-07-18 15:36:31 +1200

    Creating new Page vue element to add nav and top title. #story[490]

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,85 +1,9 @@
 <template>
   <div id="app">
-    <v-app v-if="this.$route.name == 'home'" id="inspire">
-      <router-view></router-view>
-    </v-app>
-    <v-app v-else >
-      <v-toolbar fixed app clipped-right class="main-header">
-        <router-link to="/" class="primary-logo">
-          <v-toolbar-title class="fill-height toolbar">
-            <img class="fill-height" src="./assets/logo2_white.png" />
-          </v-toolbar-title>
-        </router-link>
-      </v-toolbar>
-      <v-navigation-drawer fixed v-model="drawer" app :mini-variant.sync="mini" hide-overlay>
-        <v-toolbar flat class="transparent">
-          <v-list class="pa-0">
-            <v-list-tile avatar>
-              <v-list-tile-avatar>
-                <img src="https://randomuser.me/api/portraits/men/85.jpg" />
-              </v-list-tile-avatar>
-
-              <v-list-tile-content>
-                <v-list-tile-title>John Leider</v-list-tile-title>
-              </v-list-tile-content>
-
-              <v-list-tile-action>
-                <v-btn icon @click.stop="mini = !mini">
-                  <v-icon>chevron_left</v-icon>
-                </v-btn>
-              </v-list-tile-action>
-            </v-list-tile>
-          </v-list>
-        </v-toolbar>
-
-        <v-list class="pt-0">
-          <v-divider></v-divider>
-          <v-list-tile v-for="item in menuOptions" :key="item.name" :to="item.link">
-            <v-list-tile-action>
-              <v-icon>{{ item.icon }}</v-icon>
-            </v-list-tile-action>
-            <v-list-tile-content>{{ item.name }}</v-list-tile-content>
-          </v-list-tile>
-          <v-list-tile v-if="loggedIn" v-on:click="logout">
-            <v-list-tile-action>
-              <v-icon>lock_open</v-icon>
-            </v-list-tile-action>
-            <v-list-tile-content>Logout</v-list-tile-content>
-          </v-list-tile>
-        </v-list>
-      </v-navigation-drawer>
-      <v-content>
-        <v-container fluid fill-height ma-0 pa-0>
-          <v-layout>
-            <v-flex>
-              <router-view></router-view>
-            </v-flex>
-          </v-layout>
-        </v-container>
-      </v-content>
-    </v-app>
+    <router-view></router-view>
   </div>
 </template>
 
-
-
-<style lang="scss">
-@import "./assets/css/main.scss";
-
-.primary-logo {
-  height: 50%;
-}
-
-.v-toolbar__content {
-  display: flex;
-  justify-content: flex-end;
-}
-
-.fill-height {
-  height: 100%;
-  // align-self: right;
-}
-</style>
 
 
 <script>
