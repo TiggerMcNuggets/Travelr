c0aded1158ef4968c66d43deed8fe04c17a46153
Dana Lambert <dla72@uclive.ac.nz>
2019-07-19 20:30:27 +1200

    Adding the media grid to display image previews for uploading images. #story[490]

--- frontend/src/components/photos/PhotoUpload.vue
+++ frontend/src/components/photos/PhotoUpload.vue
@@ -7,39 +7,11 @@
         <v-card-title primary-title>
           <h2 class="headline">Upload Photo</h2>
         </v-card-title>
-
         <v-divider></v-divider>
-
-        <div id='dropzone'>Drag photos or click to open file explorer.</div>
-
-        <ul>
-          <li v-for="file in files" >
-            <!-- <div class="personal-photo-row"> -->
-              <!-- <div
-                v-for="item in row"
-                :value="item.value"
-                :key="item.value"
-                class="select-image-container"
-              >
-                <v-icon v-if="imageIsSelected(item)" class="lock-icon" left>done</v-icon>
-                <div v-if="imageIsSelected(item)" class="triangle image-selected"></div> -->
-                <v-img
-                  v-on:click="selectImage(item)"
-                  class="personal-photo-element"
-                  :src="file"
-                ></v-img>
-                  <!-- <img
-                   class="personal-photo-element"
-                  :src="file" /> -->
-                <!-- <v-img
-                  v-on:click="selectImage(item)"
-                  class="personal-photo-element"
-                  :src="(file)"
-                ></v-img> -->
-              <!-- </div> -->
-            <!-- </div> -->
-          </li>
-        </ul>
+        <div id="dropzone">Drag photos or click to open file explorer.</div>
+        <v-flex  mt-3 >
+        <MediaGrid :media="files" />
+        </v-flex>
       </div>
     </div>
     <v-divider></v-divider>
@@ -50,94 +22,17 @@
 
 
 <style>
-
-.dropzone {
-    background: white;
-    border-radius: 5px;
-    border: 2px dashed rgb(0, 135, 247);
-    border-image: none;
-    max-width: 500px;
-    margin-left: auto;
-    margin-right: auto;
-}
-.image-selected {
-  width: 0;
-  height: 0;
-  border-style: solid;
-  border-width: 70px 70px 0 0;
-  border-color: greenyellow transparent transparent transparent !important;
-  opacity: 0.3;
-  position: absolute;
-  z-index: 10;
-}
-
-.lock-icon {
-  color: white !important;
-  opacity: 1;
-  position: absolute;
-  z-index: 12;
-  font-size: 2.3em;
-  margin-top: 0.2em;
-  margin-left: 0.2em;
-  align-self: flex-start !important;
-}
-
-.select-image-container {
-  width: 24%;
-  height: 170px;
-  border: 1px solid lightgrey;
-  background-position: center;
-  padding: 7px;
-  overflow: hidden;
-  display: flex;
-  justify-content: flex-start;
-}
-
-.image-container:hover .personal-photo-element {
-  cursor: pointer;
-  opacity: 0.8;
-}
-
-.personal-photo-element {
-  height: 100%;
-  overflow: hidden;
-}
-
-.personal-photo-row {
-  display: flex;
-  justify-content: space-evenly;
-  margin-bottom: 30px;
-}
-
-ul {
-  padding-left: 0px;
-}
-
-h2 {
-  align-self: flex-end;
-}
-
-hr {
-  margin-bottom: 25px;
-}
-
 #dropzone {
-    /* background: #08c;
-    color: #fff;
-    padding: 100px 0;
-    text-align: center; */
-      background: white;
-    border-radius: 5px;
-    border: 2px dashed rgb(0, 135, 247);
-    border-image: none;
-    padding: 50px;
-    /* max-width: 500px;
-    margin-left: auto;
-    margin-right: auto; */
-  }
-  #dropzone.dragover {
-    background: rgba(0, 135, 247, 1);;
-  }
+  background: white;
+  border-radius: 5px;
+  border: 2px dashed rgb(0, 135, 247);
+  border-image: none;
+  padding: 50px;
+}
+#dropzone.dragover {
+  background: rgba(0, 135, 247, 1);
+}
+
 
 </style>
 
@@ -146,9 +41,14 @@ hr {
 import { store } from "../../store/index";
 import base_url from "../../repository/BaseUrl";
 import { getImages } from "../../repository/PersonalPhotosRepository";
+import MediaGrid from "../media/MediaGrid";
 
 export default {
   store,
+
+  components: {
+    MediaGrid
+  },
 
   // local variables
   data() {
@@ -164,8 +64,6 @@ export default {
   props: ["closeDialogue", "setDestinationImages"],
 
   methods: {
-
-    
     /**
      * Sets the user's profile photo as the selected
      */
@@ -209,7 +107,6 @@ export default {
       }
     },
 
-
     /**
      * Toggles if the image is selected and adding or removing from selected list.
      * @param selectedImage The image which was clicked.
@@ -242,104 +139,78 @@ export default {
       return newImageList;
     },
 
-  triggerCallback(e, callback) {
-    var files;
-    if(e.dataTransfer) {
-      files = e.dataTransfer.files;
-    } else if(e.target) {
-      files = e.target.files;
-    }
-    callback.call(null, files);
-  },
+    triggerCallback(e, callback) {
+      var files;
+      if (e.dataTransfer) {
+        files = e.dataTransfer.files;
+      } else if (e.target) {
+        files = e.target.files;
+      }
+      callback.call(null, files);
+    },
 
-  makeDroppable(ele, callback) {
-    // console.log("here")
-    // console.log(ele)
-    // callback("stuffs")
-      var input = document.createElement('input');
-      input.setAttribute('type', 'file');
-      input.setAttribute('multiple', true);
-      input.style.display = 'none';
-      input.addEventListener('change', (e) => {
+    makeDroppable(ele, callback) {
+      var input = document.createElement("input");
+      input.setAttribute("type", "file");
+      input.setAttribute("multiple", true);
+      input.style.display = "none";
+      input.addEventListener("change", e => {
         this.triggerCallback(e, callback);
       });
       ele.appendChild(input);
-      
-      ele.addEventListener('dragover', function(e) {
+
+      ele.addEventListener("dragover", function(e) {
         e.preventDefault();
         e.stopPropagation();
-        ele.classList.add('dragover');
+        ele.classList.add("dragover");
       });
 
-      ele.addEventListener('dragleave', function(e) {
+      ele.addEventListener("dragleave", function(e) {
         e.preventDefault();
         e.stopPropagation();
-        ele.classList.remove('dragover');
+        ele.classList.remove("dragover");
       });
 
-      ele.addEventListener('drop', (e) => {
+      ele.addEventListener("drop", e => {
         e.preventDefault();
         e.stopPropagation();
-        ele.classList.remove('dragover');
+        ele.classList.remove("dragover");
         this.triggerCallback(e, callback);
       });
-      
-      ele.addEventListener('click', function() {
+
+      ele.addEventListener("click", function() {
         input.value = null;
         input.click();
       });
     }
-
-
   },
 
   mounted: function() {
-     var dropzone = document.getElementById("dropzone");
+    var dropzone = document.getElementById("dropzone");
 
     this.makeDroppable(dropzone, files => {
-      console.log("made it to the call back yay!")
-      console.log(files);
-      // this.files = [...files].map((image) => {
-        for (let i = 0; i < files.length; i++) {
-          let file = files[i];
-          console.log(file);
-      var image = new Image();
-      var reader = new FileReader();
-      var vm = this;
+      for (let i = 0; i < files.length; i++) {
+        let file = files[i];
 
-      reader.onload = (e) => {
-        this.files.push(e.target.result);
-      };
-      reader.readAsDataURL(file);
-      // console.log(processedThing)
-      // this.files.push(processedThing);
-      };
+        var image = new Image();
+        var reader = new FileReader();
+        var vm = this;
 
-          console.log(this.files);
-      // var output = document.querySelector('.output');
-      // output.innerHTML = '';
-      // for(var i=0; i<files.length; i++) {
-      //   if(files[i].type.indexOf('image/') === 0) {
-      //     output.innerHTML += '<img width="200" src="' + URL.createObjectURL(files[i]) + '" />';
-      //   }
-      //   output.innerHTML += '<p>'+files[i].name+'</p>';
-      // }
+        reader.onload = e => {
+          this.files.push(e.target.result);
+        };
+        reader.readAsDataURL(file);
+      }
     });
   },
-  
 
   /**
    * Initialises the component with the image data.
    */
   created: function() {
-
-   
-
     this.id = this.$route.params.id;
 
-    getImages(this.id).then(result => {
-      this.files = this.groupImages(result.data);
-    });
+    this.files = [];
   }
 };
 </script>
