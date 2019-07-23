47be357625081c14e3182f8cde73bfcc4a77f809
Dana Lambert <dla72@uclive.ac.nz>
2019-07-23 12:24:54 +1200

    Adding an album repository with list and create methods. #story[450] #pair[fme36, dla72]

---
+++ app/repository/AlbumRepository.java
@@ -0,0 +1,56 @@
+package repository;
+
+import controllers.dto.Photo.UpdatePhotoReq;
+import io.ebean.Expr;
+import io.ebean.ExpressionList;
+import models.Album;
+import models.Media;
+import models.PersonalPhoto;
+import models.User;
+
+import javax.inject.Inject;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
+
+import static java.util.concurrent.CompletableFuture.supplyAsync;
+
+public class AlbumRepository {
+
+    private final DatabaseExecutionContext executionContext;
+
+    @Inject
+    public AlbumRepository(DatabaseExecutionContext executionContext) {
+        this.executionContext = executionContext;
+    }
+
+    /**
+     * Returns all photos assoociated with a user.
+     * 
+     * @param id           user id The user id
+     * @param privateMedia if true will not return the public photos that are
+     *                     available to the user
+     * @return CompletionStage<List<PersonalPhoto>> The list of personal photos
+     *         associated with the user.
+     */
+    public CompletionStage<List<Media>> list(Long id, Boolean privateMedia) {
+        return supplyAsync(() -> {
+            ExpressionList<Media> query = Media.find.query().where().eq("user_id", id).or(Expr.eq("is_public", true),
+                    Expr.eq("is_public", !privateMedia));
+            return query.findList();
+        }, executionContext);
+    }
+
+    /**
+     * Creates a new empty album
+     * 
+     * @return the new album id which was created.
+     */
+    public CompletableFuture<Long> create() {
+        return supplyAsync(() -> {
+            Album album = new Album();
+            album.save();
+            return album.id;
+        }, executionContext);
+    }
+}
