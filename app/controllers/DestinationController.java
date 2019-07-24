1a1f761912be1257c6bf90748a11d2fc6aec1701
Marvin Goesmann <mgo81@uclive.ac.nz>
2019-07-24 14:16:27 +1200

    S448 t4572 test proof of concept

--- app/controllers/DestinationController.java
+++ app/controllers/DestinationController.java
@@ -75,18 +75,19 @@ public class DestinationController extends Controller {
         CompletionStage<Result> middlewareRes = Authorization.userIdRequiredMiddlewareStack(request, userId);
         if (middlewareRes != null) return middlewareRes;
 
-        return destinationRepository.getOneDestination(destId).thenApplyAsync(destination -> {
-            // Not Found Check
-            if (destination == null) {
-                return notFound(APIResponses.DESTINATION_NOT_FOUND);
-            }
+        Destination destination = Destination.find.findByIdIncludeDeleted(destId);
+        if (destination == null) return  CompletableFuture.completedFuture(notFound(APIResponses.DESTINATION_NOT_FOUND));
 
-            Object response;
-            response = new GetDestinationsRes(destination);
-            ObjectMapper mapper = new ObjectMapper();
-            JsonNode jsonResponse = mapper.valueToTree(response);
-            return ok(jsonResponse);
-        });
+        if (!destination.getIsPublic() || (destination.getIsPublic() && destinationRepository.isDestinationUsed(destId))) {
+            CompletionStage<Result> authorisedToView = Authorization.isUserAuthorisedToViewTrip(request, userId, destination.user.id);
+            if (authorisedToView != null) return authorisedToView;
+        }
+
+        Object response;
+        response = new GetDestinationsRes(destination);
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode jsonResponse = mapper.valueToTree(response);
+        return CompletableFuture.completedFuture(ok(jsonResponse));
     }
 
     /**
@@ -200,7 +201,7 @@ public class DestinationController extends Controller {
 
             Boolean isAdmin = request.attrs().get(Attrs.IS_USER_ADMIN);
             Boolean isDestinationOwner = (destination.getUser().getId() == userId);
-            if(destination.isPublic && !isAdmin && !isDestinationOwner) {
+            if(!destination.getIsPublic() && !isAdmin && !isDestinationOwner) {
                 return CompletableFuture.completedFuture(Results.forbidden(APIResponses.FORBIDDEN_DESTINATION_EDIT));
             }
 
@@ -213,9 +214,7 @@ public class DestinationController extends Controller {
 
                 return ok("Destination updated");
             });
-
         });
-
     }
 
     /**
@@ -233,21 +232,15 @@ public class DestinationController extends Controller {
         if(updateDestinationForm.hasErrors()) {
             return CompletableFuture.completedFuture(badRequest(APIResponses.BAD_REQUEST));
         }
+        Destination destination = Destination.find.findById(id);
+        if (destination == null) return  CompletableFuture.completedFuture(notFound(APIResponses.DESTINATION_NOT_FOUND));
+
+        CompletionStage<Result> authorisedToView = Authorization.isUserAuthorisedToViewTrip(request, user.getId(), destination.user.id);
+        if (authorisedToView != null) return authorisedToView;
 
         CreateDestReq req = updateDestinationForm.get();
 
-        return destinationRepository.getOneDestination(id).thenComposeAsync(destination -> {
-            // Not Found Check
-            if (destination == null) {
-                return CompletableFuture.completedFuture(notFound(APIResponses.DESTINATION_NOT_FOUND));
-            }
-            // Forbidden Check
-            if (destination.user.id != user.id) {
-                return CompletableFuture.completedFuture(forbidden("Forbidden: Access Denied"));
-            }
-            return destinationRepository.update(req, id, user.getId()).thenApplyAsync(destId -> ok("Destination updated"));
-
-        });
+        return destinationRepository.update(req, id, user.getId()).thenApplyAsync(destId -> ok("Destination updated"));
 
     }
 
@@ -259,12 +252,17 @@ public class DestinationController extends Controller {
      */
     @Authorization.RequireAuth
     public CompletionStage<Result> toggleDestinationDeleted(Http.Request req, Long userId, Long destId) {
+
         CompletionStage<Result> middlewareRes = Authorization.userIdRequiredMiddlewareStack(req, userId);
 
         if (middlewareRes != null) return middlewareRes;
         Destination destination = Destination.find.findByIdIncludeDeleted(destId);
 
         if (destination == null) return  CompletableFuture.completedFuture(notFound(APIResponses.DESTINATION_NOT_FOUND));
+
+        CompletionStage<Result> authorisedToView = Authorization.isUserAuthorisedToViewTrip(req, userId, destination.user.id);
+        if (authorisedToView != null) return authorisedToView;
+
         return destinationRepository.toggleDestinationDeleted(destId).thenApplyAsync(deleted -> {
 
             ObjectMapper mapper = new ObjectMapper();
@@ -313,7 +311,7 @@ public class DestinationController extends Controller {
                 return forbidden("Forbidden: There are other users using the destination");
             }
 
-            destination.isPublic = false;
+            destination.setIsPublic(false);
             destination.save();
             return created("Destination is now private");
         });
--- test/resources/features/destination/getDestinations.feature
+++ test/resources/features/destination/getDestinations.feature
@@ -1,35 +1,95 @@
 Feature: GetDestinations
   Description: The purpose of this feature is to test the api endpoint related to getting destinations
 
-  Background: The database is populated
-    Given I populate the database
-    Then I will receive a 200 response
+  Scenario: Get destinations successfully
+    Given I am authenticated
+    And I own the destination
+      | name         | latitude | longitude | type     | district   | country    |
+      | Eiffel Tower | 5.0      | 5.0       | Landmark | Paris      | France     |
+    When I want to get destinations
+    And I send the request
+    Then I will receive the response code 200
+    And I will receive the response body
+      """
+      [
+        {
+          "id": 1,
+          "name": "Eiffel Tower",
+          "latitude": 5.0,
+          "longitude": 5.0,
+          "type": "Landmark",
+          "district": "Paris",
+          "country": "France",
+          "isPublic": false,
+          "ownerId": 2,
+          "travellerTypes": []
+        }
+      ]
+      """
 
-  Scenario: Get my own destinations as admin
-    Given I provide the token "123"
-    And The traveller id is 1
-    When I get destinations
-    Then I will receive a 200 response
+  Scenario: Get destinations that includes a private destination that is not mine
+    Given The user exists
+      | first | last  | email               | dob |
+      | John  | Smith | johnsmith@email.com | 1   |
+    And They own the destination
+      | name         | latitude | longitude | type     | district   | country    |
+      | Eiffel Tower | 5.0      | 5.0       | Landmark | Paris      | France     |
+    And I am authenticated
+    When I want to get destinations
+    And I send the request
+    Then I will receive the response code 200
+    And I will receive the response body
+      """
+      []
+      """
 
-  Scenario: Get others destinations as admin
-    Given I provide the token "123"
-    And The traveller id is 2
-    When I get destinations
-    Then I will receive a 200 response
+  Scenario: Get destinations that includes a public destination successfully
+    Given The user exists
+      | first | last  | email               | dob |
+      | John  | Smith | johnsmith@email.com | 1   |
+    And They own the destination
+      | name         | latitude | longitude | type     | district   | country    |
+      | Eiffel Tower | 5.0      | 5.0       | Landmark | Paris      | France     |
+    And I am authenticated
+    And The destination is public
+    When I want to get destinations
+    And I send the request
+    Then I will receive the response code 200
+    And I will receive the response body
+      """
+      [
+        {
+          "name":"Eiffel Tower",
+          "latitude":5.0,
+          "longitude":5.0,
+          "type":"Landmark",
+          "district":"Paris",
+          "country":"France",
+          "ownerId":2,
+          "isPublic":true,
+          "id":1,
+          "travellerTypes":[]
+        }
+      ]
+      """
 
-#  Scenario: Get my own destinations as normal user
-#    Given I provide the token "abc"
-#    And The traveller id is 4
-#    When I get destinations
-#    Then I will receive a 200 response
+  Scenario: Get destinations when I am not logged in
+    Given I am not authenticated
+    And I own the destination
+      | name         | latitude | longitude | type     | district   | country    |
+      | Eiffel Tower | 5.0      | 5.0       | Landmark | Paris      | France     |
+    When I want to get destinations
+    And I send the request
+    Then I will receive the response code 401
 
-  Scenario: Get others destinations as normal user
-    Given I provide the token "abc"
-    And The traveller id is 1
-    When I get destinations
-    Then I will receive a 403 response
-
-  Scenario: Get destinations without token
-    Given I provide the token "64646"
-    When I get destinations
-    Then I will receive a 401 response
+  Scenario: Get someone elses destinations
+    Given I am authenticated
+    Given The user exists
+      | first | last  | email               | dob |
+      | John  | Smith | johnsmith@email.com | 1   |
+    And They own the destination
+      | name         | latitude | longitude | type     | district   | country    |
+      | Eiffel Tower | 5.0      | 5.0       | Landmark | Paris      | France     |
+    When I want to get destinations
+    And I send the request
+    Then I will receive the response code 403
