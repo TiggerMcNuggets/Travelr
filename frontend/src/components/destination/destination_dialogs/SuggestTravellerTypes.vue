b50e845d77e6a110df32b451eb49fbc1da8979d4
Dana Lambert <dla72@uclive.ac.nz>
2019-07-24 15:47:45 +1200

    Modifying the single destination screen to use new header. #story[490]

--- frontend/src/components/destination/destination_dialogs/SuggestTravellerTypes.vue
+++ frontend/src/components/destination/destination_dialogs/SuggestTravellerTypes.vue
@@ -1,107 +1,114 @@
 <template xmlns:v-slot="http://www.w3.org/1999/XSL/Transform">
-    <v-dialog v-model="showSuggestTravellerTypes" max-width="900px">
-            <template v-slot:activator="{ on }">
-                <v-btn
-                        class="upload-toggle-button"
-                        fab
-                        small
-                        dark
-                        color="indigo"
-                        @click="toggleShowSuggestTravellerTypes"
-                >
-                    <v-icon dark>card_travel</v-icon>
-                </v-btn>
-            </template>
+  <v-dialog v-model="showSuggestTravellerTypes" max-width="900px">
+    <!-- <template v-slot:activator="{ on }">
+      <v-btn
+        class="upload-toggle-button"
+        fab
+        small
+        dark
+        color="indigo"
+        @click="toggleShowSuggestTravellerTypes"
+      >
+        <v-icon dark>card_travel</v-icon>
+      </v-btn>
+    </template>-->
 
-                <v-card min-height="400px" style="padding: 20px">
-                    <v-layout row wrap style="padding-left: 40px">
-                        <v-flex xs12 md12>
-                            <h2>Traveller Types</h2>
-                        </v-flex>
-                        <v-flex xs12 md11>
-                            <v-select
-                                    label="Associated Traveller Types"
-                                    :items="typeList"
-                                    item-text="name"
-                                    v-model="selectedTravellerTypes"
-                                    attach multiple
-                                    return-object>
-                            </v-select>
-                        </v-flex>
-                        <v-flex xs12 md11 style="text-align: center">
-                            <v-btn v-on:click="submitTravellerTypes()">
-                                Submit
-                            </v-btn>
-                        </v-flex>
-                    </v-layout>
-                </v-card>
-        </v-dialog>
+    <v-card min-height="400px" style="padding: 20px">
+      <v-layout row wrap style="padding-left: 40px">
+        <v-flex xs12 md12>
+          <h2>Traveller Types</h2>
+        </v-flex>
+        <v-flex xs12 md11>
+          <v-select
+            label="Associated Traveller Types"
+            :items="typeList"
+            item-text="name"
+            v-model="selectedTravellerTypes"
+            attach
+            multiple
+            return-object
+          ></v-select>
+        </v-flex>
+        <v-flex xs12 md11 style="text-align: center">
+          <v-btn v-on:click="submitTravellerTypes()">Submit</v-btn>
+        </v-flex>
+      </v-layout>
+    </v-card>
+  </v-dialog>
 </template>
 
 
 <script>
-    import { RepositoryFactory } from "../../../repository/RepositoryFactory";
-    let selectDataRepository = RepositoryFactory.get("selectData");
-    let destinationRepository = RepositoryFactory.get("destination");
-    import {deepCopy} from "../../../tools/deepCopy"
+import { RepositoryFactory } from "../../../repository/RepositoryFactory";
+let selectDataRepository = RepositoryFactory.get("selectData");
+let destinationRepository = RepositoryFactory.get("destination");
+import { deepCopy } from "../../../tools/deepCopy";
 
-    export default {
-        data () {
-            return {
-                showSuggestTravellerTypes: false,
-                typeList: [],
-                selectedTravellerTypes: []
-            }
-        },
-        props: {
-            destinationId: {
-                type: String,
-                required: true
-            },
-            userTravellerTypes: {
-                type: Array,
-                required: true
-            }
-        },
-        methods: {
-            /**
-             * Toggles the upload section for the photos
-             */
-            toggleShowSuggestTravellerTypes: function() {
-                this.showSuggestTravellerTypes = !this.showSuggestTravellerTypes;
-            },
-
-            /**
-             * populated list of traveller types for user to select from
-             **/
-            async populateSelects() {
-                const travellerTypes = await selectDataRepository.travellerTypes();
-                this.typeList = travellerTypes.data;
-            },
-            /**
-             * adds a request to the destination to add/remove traveller types
-             **/
-            submitTravellerTypes() {
-                let destRequest = {
-                    "destinationId": this.destinationId,
-                    "travellerTypeIds": this.selectedTravellerTypes
-                };
-                destinationRepository.addDestinationEditRequest(destRequest)
-                    .then(() => {
-                        this.showSuggestTravellerTypes = false;
-                    })
-                    .catch(err => {
-                        console.log(err);
-                    });
-            },
-        },
-
-        /**
-         * Populates the selection form items once component has mounted.
-         */
-        mounted() {
-            this.populateSelects();
-            this.selectedTravellerTypes = deepCopy(this.userTravellerTypes);
-        }
+export default {
+  data() {
+    return {
+      typeList: [],
+      selectedTravellerTypes: []
+    };
+  },
+  props: {
+    destinationId: {
+      type: String,
+      required: true
+    },
+    userTravellerTypes: {
+      type: Array,
+      required: true
+    },
+    showSuggestTravellerTypes: {
+      type: Boolean,
+      required: true
+    },
+    close: {
+      type: Function,
+      required: true
     }
+  },
+  methods: {
+    // /**
+    //  * Toggles the upload section for the photos
+    //  */
+    // toggleShowSuggestTravellerTypes: function() {
+    //   this.showSuggestTravellerTypes = !this.showSuggestTravellerTypes;
+    // },
+
+    /**
+     * populated list of traveller types for user to select from
+     **/
+    async populateSelects() {
+      const travellerTypes = await selectDataRepository.travellerTypes();
+      this.typeList = travellerTypes.data;
+    },
+    /**
+     * adds a request to the destination to add/remove traveller types
+     **/
+    submitTravellerTypes() {
+      let destRequest = {
+        destinationId: this.destinationId,
+        travellerTypeIds: this.selectedTravellerTypes
+      };
+      destinationRepository
+        .addDestinationEditRequest(destRequest)
+        .then(() => {
+          this.close();
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    }
+  },
+
+  /**
+   * Populates the selection form items once component has mounted.
+   */
+  mounted() {
+    this.populateSelects();
+    this.selectedTravellerTypes = deepCopy(this.userTravellerTypes);
+  }
+};
 </script>
