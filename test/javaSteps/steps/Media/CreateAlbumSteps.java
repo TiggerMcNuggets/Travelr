1bfa34097c48c316a05664ecbc5a5125eeed2f59
gusdaddy <fergus.meldrum@gmail.com>
2019-07-25 14:47:58 +1200

    added cucumber test for creating a destination

--- test/javaSteps/steps/Media/CreateAlbumSteps.java
+++ test/javaSteps/steps/Media/CreateAlbumSteps.java
@@ -4,7 +4,6 @@ import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import javaSteps.models.StateSingleton;
 import models.Album;
-import models.Destination;
 import org.junit.Assert;
 import play.libs.Json;
 
@@ -15,19 +14,23 @@ public class CreateAlbumSteps {
     // Singleton object that holds shared values across features
     private StateSingleton state = StateSingleton.getInstance();
 
+
     @When("I want to create an album")
-    public void iWantToCreateADestination() {
-        state.getRequest().uri((String.format("http://localhost:9000/api/users/:id/album", state.getUser().getId())));
+    public void i_want_to_create_an_album() {
+        state.getRequest().uri((String.format("http://localhost:9000/api/users/%s/albums", state.getUser().getId())));
         state.getRequest().method("POST");
     }
 
     @When("I check the album")
-    public void iCheckTheDestination() {
+    public void i_check_the_album() {
         state.setAlbum(Album.find.findAlbumById(Json.parse(contentAsString(state.getResult())).get("id").asLong()));
+
     }
 
-    @Then("The album id is {int1}")
-    public void the_album_id_is(Integer int1) {
+    @Then("The album id is {long}")
+    public void the_album_id_is(Long int1) {
+        // Write code here that turns the phrase above into concrete actions
         Assert.assertEquals(state.getAlbum().id, int1);
     }
+
 }
