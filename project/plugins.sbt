5827f84c9a604f85538337116e73a6b5aff24e98
gusdaddy <fergus.meldrum@gmail.com>
2019-07-30 12:20:43 +1200

    added changes to test media files

--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,5 +1,5 @@
 // The Play plugin
-addSbtPlugin("com.typesafe.play" % "sbt-plugin" % "2.7.0")
+addSbtPlugin("com.typesafe.play" % "sbt-plugin" % "2.7.0-RC7")
 
 // Ebean Play plugin
 addSbtPlugin("com.typesafe.sbt" % "sbt-play-ebean" % "5.0.0")
--- test/javaSteps/steps/Media/UploadMediaSteps.java
+++ test/javaSteps/steps/Media/UploadMediaSteps.java
@@ -8,11 +8,15 @@ import cucumber.api.java.bs.A;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import javaSteps.models.StateSingleton;
+import javafx.util.Pair;
 import play.libs.Files;
 import play.mvc.Http;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
 public class UploadMediaSteps {
 
@@ -33,21 +37,21 @@ public class UploadMediaSteps {
         throw new cucumber.api.PendingException();
     }
 
-//    @When("I select a valid media file")
-//    public void i_select_a_valid_media_file() {
-//        File new_media = new File("../../../resources/images/testMedia.jpg");
-//
-//
-//        Http.MultipartFormData.Part<Source<ByteString, ?>> part =
-//                new Http.MultipartFormData.FilePart<Source<ByteString, ?>>(
-//                        "picture",
-//                        "file.pdf",
-//                        "application/pdf",
-//                        FileIO.fromPath(new_media.toPath()),
-//                        Files.size(new_media.toPath()));
-//
-//        state.getRequest().bodyMultipart(part);
+    @When("I select a valid media file")
+    public void i_select_a_valid_media_file() {
+        File new_media = new File("../../../resources/images/testMedia.jpg");
 
+        List<Http.MultipartFormData.Part<Source<ByteString, ?>>> partList = new ArrayList<>();
+
+
+        partList.add(new Http.MultipartFormData.FilePart<>("picture", "test.jpg", "image/jpg",
+                FileIO.fromPath(new_media.toPath()),
+                "form-data"));
+
+
+        state.getRequest().bodyMultipart(partList,
+                play.libs.Files.singletonTemporaryFileCreator(),
+                );
 
 
 }
