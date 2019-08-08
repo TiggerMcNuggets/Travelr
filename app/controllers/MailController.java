43a184688248fa531707997a6b0ab9eb014b74b3
Flynn Doherty <frd15@uclive.ac.nz>
2019-08-08 16:44:45 +1200

    Fixed an issue in the MailController method not sending appropriately formatted requests to mailgun api. #pair[frd15, rsh134] #story[550]

--- app/controllers/MailController.java
+++ app/controllers/MailController.java
@@ -1,6 +1,7 @@
 package controllers;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import play.libs.Json;
 import play.libs.ws.WSAuthScheme;
 import play.libs.ws.WSClient;
@@ -10,32 +11,35 @@ import play.mvc.Controller;
 import play.mvc.Result;
 
 import javax.inject.Inject;
+
 import java.util.concurrent.CompletionStage;
 
 public class MailController extends Controller {
     private WSClient ws;
+
 
     @Inject
     public MailController(WSClient ws) {
         this.ws = ws;
     }
 
+
+    /**
+     * Initial implementation of sending email requests to the mailgun api.
+     * TODO: Implement dynamic fields
+     */
     public CompletionStage<Result> sendEmailGivenTrip() {
-        String reqParameters;
+        WSRequest request = ws.url("https://api.mailgun.net/v3/sandboxc7b3b2d7b248471d9e3c50aa8687d1c4.mailgun.org/messages");
 
-        reqParameters = "{" +
-            "\"from\": \"rsh134@uclive.ac.nz\"," +
-            "\"to\": \"rsh134@uclive.ac.nz\"," +
-            "\"subject\": \"JAVATEST\"," +
-            "\"text\": \"testing\"" +
-        "}";
+        request.addQueryParameter("from", "rsh134@uclive.ac.nz");
+        request.addQueryParameter("to", "rsh134@uclive.ac.nz");
+        request.addQueryParameter("subject", "JAVA TEST PLAY");
+        request.addQueryParameter("text", "QUERY PARAMS LUL");
 
-        JsonNode body = Json.parse(reqParameters);
-        WSRequest request = ws.url("https://api.mailgun.net/v3/sandboxc7b3b2d7b248471d9e3c50aa8687d1c4.mailgun.org");
-        request.addHeader("api", "369f89d26186533f02492395d4086aef-73ae490d-9c3ed2ca");
-        request.setAuth("api:", "369f89d26186533f02492395d4086aef-73ae490d-9c3ed2ca", WSAuthScheme.BASIC);
+        request.setAuth("api", "369f89d26186533f02492395d4086aef-73ae490d-9c3ed2ca", WSAuthScheme.BASIC);
 
-        CompletionStage<WSResponse> asyncResponse = request.post(body);
+        CompletionStage<WSResponse> asyncResponse = request.post("");
+
         return asyncResponse.thenApplyAsync(response -> {
             return ok();
         });
