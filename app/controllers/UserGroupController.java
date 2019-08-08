a1051c048b5704adcffc57d326bddaa384968a47
Caleb Wenborn <cwe55@uclive.ac.nz>
2019-08-08 15:38:43 +1200

    Cleaning up branch to remove other duplicated tasks from it #chore

--- app/controllers/UserGroupController.java
+++ app/controllers/UserGroupController.java
@@ -1,33 +1,10 @@
 package controllers;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ObjectNode;
-import controllers.actions.Attrs;
-import controllers.actions.Authorization;
-import controllers.constants.APIResponses;
-import controllers.dto.Destination.CreateDestReq;
-import controllers.dto.Destination.CreateDestRes;
-import controllers.dto.Destination.GetDestinationsRes;
-import controllers.dto.UserGroup.User.CreateUserGroupReq;
-import controllers.dto.UserGroup.User.CreateUserGroupRes;
-import io.ebean.Ebean;
-import models.Destination;
-import models.User;
-import models.UserGroup;
-import play.data.Form;
 import play.data.FormFactory;
 import play.mvc.Controller;
-import play.mvc.Http;
-import play.mvc.Result;
-import play.mvc.Results;
-import repository.DestinationRepository;
 import repository.UserGroupRepository;
 
 import javax.inject.Inject;
-import java.util.ArrayList;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.CompletionStage;
 
 public class UserGroupController extends Controller {
 
@@ -41,64 +18,6 @@ public class UserGroupController extends Controller {
         this.userGroupRepository = userGroupRepository;
     }
 
-    /**
-     * Creates a new userGroup
-     * @param request from http
-     * @return 201 with json object of new userGroupId if successfull
-     */
-//    @Authorization.RequireAuthOrAdmin
-    public CompletionStage<Result> createUserGroup(Http.Request request, Long userId) {
-        // Turns the post data into a form object
-        Form<CreateUserGroupReq> userGroupRequestForm = formFactory.form(CreateUserGroupReq.class).bindFromRequest(request);
-
-        // Bad Request Check
-        if (userGroupRequestForm.hasErrors()) {
-            System.out.println(userGroupRequestForm.errors());
-            return CompletableFuture.completedFuture(badRequest(APIResponses.BAD_REQUEST));
-        }
-
-        User user = User.find.findById(userId);
-
-        if (user == null) {
-            return CompletableFuture.completedFuture(notFound(APIResponses.TRAVELLER_NOT_FOUND));
-        }
-
-        CreateUserGroupReq req = userGroupRequestForm.get();
-
-        //Group Name Take Check
-        return userGroupRepository.createNewGroup(req, user).thenApplyAsync(id -> {
-            if (id == null) {
-                return badRequest("Group name is already taken");
-            }
-            CreateUserGroupRes response = new CreateUserGroupRes(id);
-            ObjectMapper mapper = new ObjectMapper();
-            JsonNode jsonResponse = mapper.valueToTree(response);
-            return created(jsonResponse);
-        });
-
-    }
-
-
-    public CompletionStage<Result> getSingleGroup(Http.Request request, Long userId, Long groupId) {
-        return userGroupRepository.getOneGroup(groupId).thenApplyAsync(UserGroups -> {
-            for (UserGroup user: UserGroups) {
-                user.getUser();
-            }
-            return ok();
-        });
-    }
-
-//    public CompletionStage<Result> getAllGroups() {
-//        return userGroupRepository.getAllGroups().thenApplyAsync(groups -> {
-//
-//            GetUserGroupRes response = new GetUserGroupRes(groups);
-//            ObjectMapper mapper = new ObjectMapper();
-//            JsonNode jsonResponse = mapper.valueToTree(response.getGetUserRes());
-//
-//            return ok(jsonResponse);
-//
-//        });
-//    }
 
 
 }
--- app/repository/UserGroupRepository.java
+++ app/repository/UserGroupRepository.java
@@ -1,13 +1,6 @@
 package repository;
 
-import controllers.dto.UserGroup.User.CreateUserGroupReq;
-import finders.UserGroupFinder;
-import models.*;
-
 import javax.inject.Inject;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.CompletableFuture;
 
 import static java.util.concurrent.CompletableFuture.supplyAsync;
 
@@ -15,48 +8,9 @@ public class UserGroupRepository {
 
     private DatabaseExecutionContext context;
 
-    private UserGroupFinder userGroupFinder = new UserGroupFinder();
-
     @Inject
     public UserGroupRepository(DatabaseExecutionContext context) {
         this.context = context;
     }
 
-
-
-    /**
-     * Creates a new group for the user provided
-     * @param request CreateUserGroupReq with name and description
-     * @param user The user who the group is being created for
-     * @return the groups Id
-     */
-    public CompletableFuture<Long> createNewGroup(CreateUserGroupReq request, User user) {
-        return supplyAsync(() -> {
-            Grouping group = new Grouping(request.name, request.description);
-
-            UserGroup userGroup = new UserGroup(user, group, true);
-
-            userGroup.insert();
-
-            return userGroup.id;
-
-        }, context);
-    }
-
-    /**
-     * Gets all userGroups
-     * @return completable future of list of userGroups
-     */
-    public CompletableFuture<List<UserGroup>> getAllGroups() {
-        return supplyAsync(() -> userGroupFinder.findAll(), context);
-    }
-
-    /**
-     * Gets a single user group
-     * @return completable future of list of userGroups
-     */
-    public CompletableFuture<List<UserGroup>> getOneGroup(Long id) {
-            return supplyAsync(() -> UserGroup.find.query().where().eq("group_id",id).findList(), context);
-//        return supplyAsync(() ->  UserGroup.find.byId(id), context);
-    }
 }
