499049d7f658670bf360e2de39a43df16abc49f0
Dana Lambert <dla72@uclive.ac.nz>
2019-08-08 19:49:11 +1200

    Adding tests for delete group member endpoint and added common test steps #story[548]

---
+++ test/javaSteps/steps/userGroups/CommonUserGroupSteps.java
@@ -0,0 +1,51 @@
+package javaSteps.steps.userGroups;
+
+import cucumber.api.java.en.Given;
+import javaSteps.models.StateSingleton;
+import models.*;
+
+import java.util.List;
+import java.util.Map;
+
+public class CommonUserGroupSteps {
+
+    // Singleton object that holds shared values across features
+    private StateSingleton state = StateSingleton.getInstance();
+
+    @Given("A group exists")
+    public void an_group_exists() {
+        Grouping group = new Grouping("Team 300", "The best team eva");
+        group.insert();
+        state.setGroup(group);
+    }
+
+    @Given("I own the user group")
+    public void i_own_the_user_group(List<Map<String, String>> dataTable) {
+        Map<String, String> groupInfo = dataTable.get(0);
+        Grouping group = new Grouping(groupInfo.get("name"), groupInfo.get("description"));
+        group.insert();
+        state.setGroup(group);
+
+        UserGroup userGroup = new UserGroup(state.getUser(), group, true);
+        userGroup.insert();
+    }
+
+    @Given("the user group has the group member")
+    public void the_user_group_has_the_group_member(List<Map<String, String>> dataTable) {
+        Map<String, String> userInfo = dataTable.get(0);
+
+        User newGroupMember = new User(
+                userInfo.get("first"),
+                userInfo.get("last"),
+                userInfo.get("email"),
+                Integer.valueOf(userInfo.get("dob"))
+        );
+        newGroupMember.insert();
+        state.setGroupMember(newGroupMember);
+        Grouping group = state.getGroup();
+
+        UserGroup userGroup = new UserGroup(newGroupMember, group, false);
+        userGroup.insert();
+    }
+
+}
---
+++ test/javaSteps/steps/userGroups/DeleteUserGroupMemberSteps.java
@@ -0,0 +1,40 @@
+package javaSteps.steps.userGroups;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import javaSteps.models.StateSingleton;
+import models.User;
+import models.UserGroup;
+import org.junit.Assert;
+
+public class DeleteUserGroupMemberSteps {
+
+    // Singleton object that holds shared values across features
+    private StateSingleton state = StateSingleton.getInstance();
+
+    @Given("the user group does not have another group member")
+    public void the_user_group_does_not_have_another_group_member() {
+        User newGroupMember = new User(
+                "Invisible",
+                "Person",
+                "nothing@nothing.com",
+                0
+        );
+        newGroupMember.insert();
+        state.setGroupMember(newGroupMember);
+    }
+
+    @When("I want to remove the group member in the user group")
+    public void i_want_to_remove_the_group_member_in_the_user_group() {
+        state.getRequest().method("DELETE");
+        state.getRequest().uri(String.format("https://localhost:9000/api/users/%s/group/%s/member/%s", state.getUser().getId(), state.getGroup().getId(), state.getGroupMember().getId()));
+    }
+
+    @Then("The group member does not exist in the user group")
+    public void the_group_member_does_not_exist_in_the_user_group() {
+        UserGroup deletedGroupMember = UserGroup.find.query().where().eq("user_id", state.getGroupMember().getId()).findOne();
+        Assert.assertNull(deletedGroupMember);
+    }
+
+}
