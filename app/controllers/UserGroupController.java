49042bc3ad2aca9dcfffe577e33c71693ea02a61
Dana Lambert <dla72@uclive.ac.nz>
2019-08-10 10:26:23 +1200

    Fixing one more snake case which was missed.

--- app/controllers/UserGroupController.java
+++ app/controllers/UserGroupController.java
@@ -22,21 +22,21 @@ public class UserGroupController extends Controller {
     /**
      * Deletes a single group member
      * @param request the http request
-     * @param user_id the user id
+     * @param userId the user id
      * @param groupId the group id for the member to be deleted from
      * @param memberId the member id to be deleted
      * @return result of operation
      */
     @Authorization.RequireAuth
-    public CompletionStage<Result> deleteGroupMember(Http.Request request, Long user_id, Long groupId, Long memberId) {
+    public CompletionStage<Result> deleteGroupMember(Http.Request request, Long userId, Long groupId, Long memberId) {
         // middleware stack
-        CompletionStage<Result> middlewareRes = Authorization.userIdRequiredMiddlewareStack(request, user_id);
+        CompletionStage<Result> middlewareRes = Authorization.userIdRequiredMiddlewareStack(request, userId);
         if (middlewareRes != null)
             return middlewareRes;
 
-        return userGroupRepository.remove(groupId, memberId).thenApplyAsync(deleted_user_id -> {
+        return userGroupRepository.remove(groupId, memberId).thenApplyAsync(deleted_userId -> {
             //not found check, repository checks that both album and media exist
-            if(deleted_user_id == null) {
+            if(deleted_userId == null) {
                 return notFound(APIResponses.GROUP_MEMBER_NOT_FOUND);
             }
             return ok(APIResponses.SUCCESSFUL_GROUP_MEMBER_DELETION);
