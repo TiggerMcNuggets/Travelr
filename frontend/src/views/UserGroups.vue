502f86b8bfd8e6341b4aa2e0e6053e15be80a11a
Lorenzo <faso.9603@gmail.com>
2019-08-18 14:46:46 +1200

    Added Javadoc to methods I created
    #story[549]

--- frontend/src/views/UserGroups.vue
+++ frontend/src/views/UserGroups.vue
@@ -126,6 +126,12 @@ export default {
       this.rollbackRedo(actions);
     },
 
+    /**
+     * Callback for rollback mixin rollbackCheckpoint function
+     * @param {string} type The original action http method
+     * @param {url: string, body: Object} actionBody The url and json body for the action request
+     * @param {url: string, body: Object} reactionBody The url and json body for the reaction request
+     */
     checkpoint: function(type, action, reaction) {
         this.rollbackCheckpoint(type, action, reaction);
     }
