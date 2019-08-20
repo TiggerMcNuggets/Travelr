5444a44052534b1594f7928474672884e9ae5c2c
Dana Lambert <dla72@uclive.ac.nz>
2019-08-20 20:52:26 +1200

    Removing system log statements.

--- app/repository/AlbumRepository.java
+++ app/repository/AlbumRepository.java
@@ -35,8 +35,8 @@ public class AlbumRepository {
      * @param albumId      user id The user id
      * @param privateMedia if true will not return the public photos that are
      *                     available to the user
-     * @return CompletionStage<List<Media>> The list of personal photos
-     *         associated with the user.
+     * @return CompletionStage<List<Media>> The list of personal photos associated
+     *         with the user.
      */
     public CompletableFuture<List<Media>> list(Long albumId, Boolean privateMedia) {
         return supplyAsync(() -> {
@@ -44,8 +44,6 @@ public class AlbumRepository {
                     .or(Expr.eq("is_public", true), Expr.eq("is_public", !privateMedia));
 
             List<Media> mediaList = query.findList();
-            System.out.println("mediaList");
-            System.out.println(mediaList);
             return mediaList;
         }, executionContext);
     }
@@ -66,7 +64,8 @@ public class AlbumRepository {
                 }
             }
 
-            if (user == null || duplicateName) return null;
+            if (user == null || duplicateName)
+                return null;
 
             Album album = new Album(user, name, false);
             album.save();
@@ -100,17 +99,19 @@ public class AlbumRepository {
     public CompletionStage<List<Album>> listUserAlbums(Long userId) {
         return supplyAsync(() -> {
             User user = User.find.findById(userId);
-            if (user == null) return null;
+            if (user == null)
+                return null;
             return user.getAlbums();
         }, executionContext);
     }
 
     /**
      * Updates an album
-     * @param userId The user's id
+     * 
+     * @param userId  The user's id
      * @param albumId The album's id
      * @param isAdmin Whether the user is an admin
-     * @param req The request object
+     * @param req     The request object
      * @return
      */
     public CompletableFuture<Album> updateAlbum(Long albumId, Long userId, boolean isAdmin, CreateAlbumReq req) {
@@ -118,7 +119,8 @@ public class AlbumRepository {
             Album album = Album.find.findAlbumById(albumId);
 
             // Not found check
-            if (album == null) return null;
+            if (album == null)
+                return null;
 
             // Forbidden check
             boolean forbidden = !isAdmin && album.getUser().getId() != userId;
