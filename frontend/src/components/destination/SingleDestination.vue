39d4c9eb74ca8c52c09525ba04d6105005dbbbb3
Dana Lambert <dla72@uclive.ac.nz>
2019-08-20 13:32:34 +1200

    Adding photo select and changing modifying destination page to use new media #story[554]

--- frontend/src/components/destination/SingleDestination.vue
+++ frontend/src/components/destination/SingleDestination.vue
@@ -1,114 +1,57 @@
 <template>
   <v-container fluid>
-    <PageHeader :title="destination.name" disableUndoRedo enableBackButton :options="options"/>
+    <PageHeader :title="destination.name" disableUndoRedo enableBackButton :options="options" />
 
-    <div class="dest-sub-info">
-      <p>
-        <strong>Type:</strong>
-        {{destination.type}}
-      </p>
-      <p>
-        <strong>District:</strong>
-        {{destination.district}}
-      </p>
-      <p>
-        <strong>Country:</strong>
-        {{destination.country}}
-      </p>
-      <p>
-        <strong>Latitude:</strong>
-        {{destination.latitude}}
-      </p>
-      <p>
-        <strong>Longitude:</strong>
-        {{destination.longitude}}
-      </p>
-      <p>
-        <strong>Traveller Types:</strong>
-        {{getTravellerTypes(destination.travellerTypes)}}
-      </p>
-    </div>
-    <v-divider class="photo-header-divider"></v-divider>
-    <div v-if="showUploadSection">
-      <div class="upload-section section">
-        <label>
-          <input
-            class="choose-file-button"
-            type="file"
-            id="file"
-            ref="file"
-            v-on:change="handleFileUpload()"
-          >
-        </label>
-        <div>
-          <v-btn v-on:click="submitFile()">Upload Photo</v-btn>
-        </div>
-      </div>
-      <v-alert :value="uploadError" color="error">{{errorText}}</v-alert>
-      <v-alert :value="uploadSuccessful" color="success">Upload Successful</v-alert>
-      <v-divider class="photo-header-divider"></v-divider>
-    </div>
-
-    <ul>
-      <li v-for="row in files" :value="row.value" :key="row.value">
-        <div class="personal-photo-row">
-          <div v-for="item in row" :value="item.value" :key="item.value" class="image-container">
-            <v-icon v-if="item.is_public" class="lock-icon" left>lock_open</v-icon>
-            <v-icon v-else class="lock-icon" left>lock</v-icon>
-
-            <div v-if="item.is_public" class="triangle pink-color"></div>
-            <div v-else class="triangle"></div>
-
-            <v-img
-              @click.stop="dialog = true"
-              v-on:click="setDialogContent(item)"
-              class="personal-photo-element"
-              :src="getImgUrl(item)"
-            ></v-img>
+    <v-layout row wrap>
+      <v-flex md2 pr-3>
+        <div class>
+          <div class="field-section">
+            <p class="caption font-weight-light field-title">DESTINATION TYPE</p>
+            <p class="body-1 font-weight-normal">{{destination.type}}</p>
+          </div>
+          <div class="field-section">
+            <p class="caption font-weight-light field-title">DISTRICT</p>
+            <p class="body-1 font-weight-normal">{{destination.district}}</p>
+          </div>
+          <div class="field-section">
+            <p class="caption font-weight-light field-title">COUNTRY</p>
+            <p class="body-1 font-weight-normal">{{destination.country}}</p>
+          </div>
+          <div class="field-section">
+            <p class="caption font-weight-light field-title">LATITUDE</p>
+            <p class="body-1 font-weight-normal">{{destination.latitude}}</p>
+          </div>
+          <div class="field-section">
+            <p class="caption font-weight-light field-title">LONGITUDE</p>
+            <p class="body-1 font-weight-normal">{{destination.longitude}}</p>
+          </div>
+          <div class="field-section">
+            <p class="caption font-weight-light field-title">TRAVELLER TYPES</p>
+            <p class="body-1 font-weight-normal">{{getTravellerTypes(destination.travellerTypes)}}</p>
           </div>
         </div>
-      </li>
-    </ul>
+      </v-flex>
+      <v-flex md10>
+        <MediaGrid
+          :filteredMedia="files"
+          :openElement="() => {}"
+          :getAllAlbums="() => {}"
+          :openEditAlbumDialog="() => {}"
+        ></MediaGrid>
+      </v-flex>
 
-    <suggest-traveller-types
-      :destinationId="destId"
-      :userTravellerTypes="[]"
-      :showSuggestTravellerTypes="showSuggestTravellerTypes"
-      :close="toggleShowSuggestTravellerTypes"
-    />
-    <v-dialog v-model="chooseExistingDialog" width="800">
-      <PhotoSelect v-bind="{closeDialog, setDestinationImages}"/>
-    </v-dialog>
+      <suggest-traveller-types
+        :destinationId="destId"
+        :userTravellerTypes="[]"
+        :showSuggestTravellerTypes="showSuggestTravellerTypes"
+        :close="toggleShowSuggestTravellerTypes"
+      />
+      <v-dialog v-model="chooseExistingDialog" width="800">
+        <PhotoSelect :closeDialog="closeDialog" :setDestinationImages="setDestinationImages" />
+      </v-dialog>
+    </v-layout>
   </v-container>
 </template>
-
-<style>
-@import "../../assets/css/style.css";
-.dest-name {
-  display: flex;
-  justify-content: space-between;
-  align-items: flex-start;
-}
-
-.dest-name div {
-  text-align: start;
-}
-
-.dest-sub-info {
-  display: flex;
-  justify-content: flex-start;
-  align-items: center;
-}
-.dest-sub-info p {
-  margin-right: 20px;
-  color: grey;
-}
-
-.dest-name button {
-  margin-right: 20px;
-}
-</style>
-
 
 <script>
 import { RepositoryFactory } from "../../repository/RepositoryFactory";
@@ -116,25 +59,19 @@ import base_url from "../../repository/BaseUrl";
 import PhotoSelect from "../photos/PhotoSelect";
 import SuggestTravellerTypes from "./destination_dialogs/SuggestTravellerTypes";
 import { store } from "../../store/index";
-import {
-  storeDestinationImage,
-  getImages,
-  updateDestinationPhoto,
-  addExistingPhoto
-} from "../../repository/DestinationPhotoRepository";
 import PageHeader from "../common/header/PageHeader";
+import MediaGrid from "../media/MediaGrid";
 
 let mediaRepository = RepositoryFactory.get("media");
-
 let destinationRepository = RepositoryFactory.get("destination");
 
 export default {
   store,
-
   components: {
     PhotoSelect,
     SuggestTravellerTypes,
-    PageHeader
+    PageHeader,
+    MediaGrid
   },
 
   // local variables
@@ -142,13 +79,7 @@ export default {
     return {
       files: [],
       TravellerTypes: [],
-      typeList: [],
-      clickedImageURL: "",
-      clickedImage: {},
-      clickedImageWidth: 0,
       dialog: false,
-      publicPhotoSwitch: false,
-      showUploadSection: false,
       userId: null,
       isMyProfile: false,
       isAdminUser: false,
@@ -178,11 +109,6 @@ export default {
       }
 
       if (this.isMyProfile || this.isAdminUser) {
-        options.push({
-          action: this.toggleShowUploadPhoto,
-          icon: "insert_photo"
-        });
-
         options.push({
           action: () => {
             this.chooseExistingDialog = true;
@@ -229,19 +155,18 @@ export default {
      * @param selectedImages The photo details for all the photos selected by the user.
      */
     setDestinationImages(selectedImages) {
-      for (let i = 0; i < selectedImages.length; i++) {
+      selectedImages.forEach(image => {
         mediaRepository
           .moveMediaToAlbum(
-            this.userId,
+            this.$store.getters.getUser.id,
             this.destination.defaultAlbumId,
-            selectedImages[i].id
+            image.id
           )
+          .catch(error => console.log(error))
           .then(() => {
-            getImages(this.userId, this.destId).then(result => {
-              this.files = this.groupImages(result.data);
-            });
+            this.getDestinationImages();
           });
-      }
+      });
       this.closeDialog();
     },
 
@@ -260,103 +185,12 @@ export default {
     },
 
     /**
-     * Closes the choose existing photo dialog.
-     */
-    closeMediaDialog() {
-      this.dialog = false;
-    },
-
-    /**
-     * Sets the file property the the file being uploaded.
-     */
-    handleFileUpload() {
-      this.file = this.$refs.file.files[0];
-    },
-
-    /**files
-     * Toggles the upload section for the photos
-     */
-    toggleShowUploadPhoto: function() {
-      this.showUploadSection = !this.showUploadSection;
-    },
-
-    /**
-     * Updates whether the photo is public or private depending on the switch state.
-     */
-    updatePhotoVisability() {
-      this.clickedImage.is_public = this.publicPhotoSwitch;
-      updateDestinationPhoto(this.clickedImage);
-    },
-
-    /**
-     * Submits the image file and uploads it to the server
-     */
-    submitFile() {
-      this.uploadError = false;
-      this.uploadExisting = false;
-      let formData = new FormData();
-      formData.append("picture", this.file);
-
-      storeDestinationImage(this.userId, this.destId, formData)
-        .then(() => {
-          getImages(this.userId, this.destination.defaultAlbumId).then(
-            result => {
-              this.uploadExisting = true;
-              console.log(result);
-              this.files = this.groupImages(result.data);
-            }
-          );
-        })
-        .catch(() => {
-          this.uploadError = true;
-        });
-      this.$refs.file.value = "";
-    },
-
-    /**
      * Gets the image src url for the server
      * @param item The photo item to get the image for.
      * @returns {string} A url of where to find the photo to set as src
      */
     getImgUrl(item) {
       return `${base_url}/api/users/${this.userId}/media/${item.uriString}`;
-    },
-
-    /**
-     * Sets the width of the dialog based on the image width.
-     * @param selectedImage The image which has been clicked on.
-     */
-    setDialogContent(selectedImage = "") {
-      this.dialog = true;
-      this.clickedImage = selectedImage;
-      this.publicPhotoSwitch = selectedImage.is_public;
-      this.clickedImageURL = this.getImgUrl(selectedImage);
-      const myImage = new Image();
-      myImage.src =
-        base_url + "/api/destinations/photo/" + selectedImage.photo_filename;
-      this.clickedImageWidth = myImage.width < 400 ? 400 : myImage.width;
-    },
-
-    /**
-     * Groups the images into rows with four columns.
-     * @param imageList The list of image data from the server.
-     * @returns {Array} A list of rows each with four images.
-     */
-    groupImages(imageList) {
-      let newImageList = [];
-      let row = [];
-      const num_cols = 4;
-      for (let i = 0; i < imageList.length; i++) {
-        if (i % num_cols === 0 && row.length !== 0) {
-          newImageList.unshift(row);
-          row = [];
-        }
-        row.push(imageList[i]);
-      }
-
-      newImageList.unshift(row);
-      newImageList.reverse();
-      return newImageList;
     },
 
     /**
@@ -375,6 +209,23 @@ export default {
       } else {
         return "-";
       }
+    },
+
+    /**
+     * Gets all the destination photos within the destionation album
+     */
+    getDestinationImages() {
+      mediaRepository
+        .getAlbumContent(
+          this.$store.getters.getUser.id,
+          this.destination.defaultAlbumId
+        )
+        .then(response => {
+          response.data.forEach(item => {
+            item.filename = item.uriString;
+          });
+          this.files = response.data;
+        });
     }
   },
 
@@ -392,15 +243,12 @@ export default {
     this.isMyProfile = store.getters.getUser.id == this.userId;
     this.isAdminUser = store.getters.getIsUserAdmin;
 
-    // Gets the information relating to selected destination.
     destinationRepository
       .getDestination(this.userId, this.destId)
       .then(response => {
         this.destination = response.data;
         // Gets all the images to display on the page.
-        getImages(this.userId, this.destination.defaultAlbumId).then(result => {
-          this.files = this.groupImages(result.data);
-        });
+        this.getDestinationImages();
       })
       .catch(err => {
         console.log(err);
