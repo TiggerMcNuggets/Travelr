2c5a8ab6066206b39d3403421814b2a0921afa76
gusdaddy <fergus.meldrum@gmail.com>
2019-08-21 13:12:43 +1200

    added cucumber tests for create trip endpoint

--- test/javaSteps/steps/trips/CreateTripsSteps.java
+++ test/javaSteps/steps/trips/CreateTripsSteps.java
@@ -1,11 +1,15 @@
 package javaSteps.steps.trips;
 
 import cucumber.api.java.en.When;
+import finders.NodeFinder;
+import finders.TripNodeFinder;
 import javaSteps.models.StateSingleton;
+import models.TripNode;
 
 public class CreateTripsSteps {
 
     StateSingleton state = StateSingleton.getInstance();
+    TripNodeFinder tripNodeFinder = new TripNodeFinder();
 
     @When("I want to create a trip")
     public void i_want_to_create_a_trip() {
@@ -13,4 +17,12 @@ public class CreateTripsSteps {
         state.getRequest().method("POST");
     }
 
+    @When("I want to add destinations to my trip which has id {int}")
+    public void i_want_to_add_destinations_to_my_trip_which_had_id(int int1) {
+        Long tripId = new Long(int1);
+        TripNode createdTrip = TripNode.find.byId(tripId);
+        state.setTrip(createdTrip);
+        state.getRequest().uri((String.format("http://localhost:9000/api/users/%s/trips/%s", state.getUser().getId(), state.getTrip().getId())));
+        state.getRequest().method("PUT");
+    }
 }
