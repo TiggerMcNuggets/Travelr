35536ba49e4078c3119ad08699c9c8f6549ff40c
Caleb Wenborn <cwe55@uclive.ac.nz>
2019-09-06 17:51:01 +1200

    Continued to create proof of concept of how PDF Itenarary may work. #story[s601]

--- app/utils/PDFCreator.java
+++ app/utils/PDFCreator.java
@@ -15,13 +15,38 @@ public class PDFCreator {
         Document doc = null;
         try {
             //Grabs the html file for trip email to be converted to a string
-            File input = new File("resources/pdfs/trip_email_title.html");
+            File input = new File("resources/templates/trip_template_test.html");
+            System.out.println(input.toString());
             doc = Jsoup.parse(input, "UTF-8", "http://example.com/");
         } catch (IOException e) {
             e.printStackTrace();
         }
+        String html_template = doc.toString();
         //Returns a string representation of the html for trip email to be converted to a pdf
-        return generatePDF(doc.toString());
+        String destinations_and_details = "<tr>\n" +
+                "<td style=\"width: 25%; text-align: center;\">North Pole</td>\n" +
+                "<td style=\"width: 25%; text-align: center;\">24-12-2019</td>\n" +
+                "<td style=\"width: 25%; text-align: center;\">26-12-2019</td>\n" +
+                "<td style=\"width: 25%; text-align: center;\">Santa doesn't exist</td>\n" +
+                "</tr>";
+        html_template = html_template.replace("destinations_and_details", destinations_and_details);
+        String start_date = "<td style=\"width: 65.9508%; text-align: center;\"><span>30-07-1998</span></td>";
+        html_template = html_template.replace("start_date", start_date);
+        String end_date = "<td style=\"width: 65.9508%; text-align: center;\"><span>25-12-2019</span></td>";
+        html_template = html_template.replace("end_date", end_date);
+        String num_of_travellers = "<td style=\"width: 65.9508%; text-align: center;\"><span>3</span></td>";
+        html_template = html_template.replace("num_of_travellers", num_of_travellers);
+        String traveller_list = "<tr>\n" +
+                                    "<td>Tiger Huang</td>\n" +
+                                    "<td>09-09-1998</td>\n" +
+                                "</tr>\n" +
+                                "<tr>\n" +
+                                    "<td>Dana Lambert</td>\n" +
+                                    "<td>13-07-1998</td>\n" +
+                                "</tr>";
+        html_template = html_template.replace("traveller_list", traveller_list);
+//        return generatePDF(doc.toString());
+        return generatePDF(html_template);
     }
 
     private File generatePDF(String htmlTemplate) {
