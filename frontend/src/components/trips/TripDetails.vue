2d786d1e03501c9d0e0618ddc0d1dcf9e9bea838
Dana Lambert <dla72@uclive.ac.nz>
2019-09-08 16:19:00 +1200

    Adding in trip overview components with album comment and status components. #story[553]

---
+++ frontend/src/components/trips/TripDetails.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-container fluid>
+    <v-tabs v-model="active" slider-color="blue">
+      <v-tab :key="1" ripple>Overview</v-tab>
+      <v-tab :key="2" ripple>Comments</v-tab>
+      <v-tab :key="3" ripple>Files</v-tab>
+
+      <v-tab-item :key="1">
+        <TripOverview/>
+      </v-tab-item>
+
+      <v-tab-item :key="2">
+        <TripComments/>
+      </v-tab-item>
+
+      <v-tab-item :key="3">
+        <TripAlbum/>
+      </v-tab-item>
+    </v-tabs>
+  </v-container>
+</template>
+
+<style>
+</style>
+
+<script>
+import PageHeader from "../common/header/PageHeader";
+import TripOverview from "./TripOverview";
+import TripComments from "./TripComments";
+import TripAlbum from "./TripAlbum";
+
+export default {
+  name: "Trip Details",
+
+  data() {
+    return {
+      active: null
+    };
+  },
+
+  components: {
+    PageHeader,
+    TripOverview,
+    TripComments,
+    TripAlbum
+  },
+
+  methods: {}
+};
+</script>
---
+++ frontend/src/components/trips/TripOverview.vue
@@ -0,0 +1,23 @@
+
+
+<template>
+  <v-layout row wrap>
+    <v-flex xs12 ma-2>
+      <h3>Status Stuff</h3>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  name: "Trip Overview",
+
+  props: {},
+
+  data() {
+    return {};
+  },
+
+  methods: {}
+};
+</script>
