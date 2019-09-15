5f4d87bc10f73e86637b8bb15b9aa78cb14f7f5b
Marvin <mgo81@uclive.ac.nz>
2019-09-15 15:49:20 +1200

    fixed delete comment test method formatting

--- test/javaSteps/steps/comments/DeleteCommentSteps.java
+++ test/javaSteps/steps/comments/DeleteCommentSteps.java
@@ -10,13 +10,11 @@ public class DeleteCommentSteps {
     @When("I want to delete the comment")
     public void i_delete_the_comment() {
         state.getRequest()
-                .uri(String
-                        .format("http://localhost:9000/api/users/%s/trips/%s/comments/%s/toggle_deleted",
-                                state.getUser().getId().toString(),
-                                state.getTrip().getId().toString(),
-                                state.getComment().getId().toString()
-                        )
-                );
+             .uri(String.format("http://localhost:9000/api/users/%s/trips/%s/comments/%s/toggle_deleted",
+                 state.getUser().getId().toString(),
+                 state.getTrip().getId().toString(),
+                 state.getComment().getId().toString())
+             );
         state.getRequest().method("PUT");
     }
 }
