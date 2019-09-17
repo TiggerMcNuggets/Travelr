c61ed03a5c4d2762a034e2e2d6c68c5ccd90d9a9
Caleb Wenborn <cwe55@uclive.ac.nz>
2019-09-17 21:30:02 +1200

    Added ability to click on media item within the trip album to expand it #implement #story[603]

--- frontend/src/components/trips/TripAlbum.vue
+++ frontend/src/components/trips/TripAlbum.vue
@@ -4,19 +4,31 @@
       <v-flex >
         <MediaGrid
           :filteredMedia="files"
-          :openElement="() => {}"
+          :openElement="openImage"
           :getAllAlbums="() => {}"
           :openEditAlbumDialog="() => {}"
           smallGrid
         ></MediaGrid>
       </v-flex>
     </v-flex>
+    <v-dialog v-model="viewMediaDialogActive" :width="clickedImageWidth">
+      <MediaDialog
+        :clickedImage="clickedImage ? clickedImage : {}"
+        :closeMediaDialog="() => viewMediaDialogActive = false"
+        :deleteMedia="deleteMedia"
+        :openConfirmDelete="() => {confirmDeletionDialogActive = true}"
+        :isMyProfile="isMyProfile"
+        :isAdminUser="isAdminUser"
+      />
+    </v-dialog>
   </v-layout>
 </template>
 
 <script>
 import { RepositoryFactory } from "../../repository/RepositoryFactory";
 import MediaGrid from "../media/MediaGrid";
+import MediaDialog from "../media/MediaDialog";
+import base_url from "../../repository/BaseUrl";
 let mediaRepository = RepositoryFactory.get("media");
 
 export default {
@@ -24,6 +36,7 @@ export default {
 
   components: {
     MediaGrid,
+    MediaDialog
   },
 
   props: {
@@ -32,6 +45,11 @@ export default {
 
   data() {
     return {
+      clickedImageWidth: 0,
+      viewMediaDialogActive: false,
+      clickedImage: null,
+      isMyProfile: true,
+      isAdminUser: true,
       files: [],
       };
   },
@@ -43,6 +61,24 @@ export default {
   },
 
   methods: {
+    deleteMedia() {
+      console.log("Not sure here");
+    },
+    deleteAndCloseDialog(deleteAll) {
+      this.confirmDeletionDialogActive = false;
+      this.deleteMedia(this.clickedImage, deleteAll);
+      this.viewMediaDialogActive = false;
+    },
+    openImage(image) {
+        // Trying to open an photo/video/media
+        const myImage = new Image();
+        myImage.src =
+          base_url +
+          `/api/users/${this.$route.params.id}/media/${image.filename}`;
+        this.clickedImageWidth = myImage.width < 400 ? 400 : myImage.width;
+        this.clickedImage = image;
+        this.viewMediaDialogActive = true;
+    },
     /**
      * Gets all the trip photos within the trip album
      */
