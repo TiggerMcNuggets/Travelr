9874a25a97e6429e2f7eb9bbf104e9cdfbaebd58
gusdaddy <fergus.meldrum@gmail.com>
2019-09-19 14:44:21 +1200

    got destination locations showing on map, in the process of getting path between locations showing
    -need to get red markers for trips
    -need to get API key reconnected

--- frontend/src/components/trips/TripMap.vue
+++ frontend/src/components/trips/TripMap.vue
@@ -10,18 +10,18 @@
         <!-- Private destination markers -->
 
         <GmapMarker
-          v-for="(destination, i) in listOfDestinations"
+          v-for="(destination, i) in allMarkers"
           :key="i"
-          :position="destination"
+          :position="destination.position"
           :draggable="false"
-          :icon="blueMarker"
+          :icon="destination.icon"
         />
-        <GmapPolyline
-          v-for="path in paths"
-          :key="path.id"
-          :options="{path: path, strokeColor: path[0].strokeColor}"
-        >
-        </GmapPolyline>
+        <!--<GmapPolyline-->
+          <!--v-for="path in paths"-->
+          <!--:key="path.id"-->
+          <!--:options="{path: path, strokeColor: path[0].strokeColor}"-->
+        <!--&gt;-->
+        <!--</GmapPolyline>-->
       </GmapMap>
     </v-flex>
   </v-layout>
@@ -87,6 +87,52 @@
           return accumulator.concat([...path])
         }, []);
       },
+      /**
+       * creates all the location markers for the map
+       */
+      allMarkers() {
+        let mapMarkers = [];
+        for (let i = 0; i < this.nodes.length; i++) {
+          let singleMarker = new Object();
+          if (this.nodes[i].type === "destination") {
+              let markerPosition = {
+              lat: this.nodes[i].destination.latitude,
+              lng: this.nodes[i].destination.longitude
+              };
+              singleMarker.destination = this.nodes[i];
+              singleMarker.position = markerPosition;
+              //add line here to give it a red marker if adjacent to a trip
+              //if (this.isAdjacentToTrip(i, this.nodes)) {
+              singleMarker.icon = this.blueMarker;
+              mapMarkers.push(singleMarker)
+          }
+        }
+        return mapMarkers;
+      },
+      /**
+       * creates all the paths between locations for the map
+       */
+      allPaths() {
+        let paths = [];
+        //paths are created from current destination location and the one previous, why list starts from second element
+        //here
+        for (let i = 1; i < this.nodes.length; i++) {
+          let singlepath = new Object();
+          if (this.nodes[i].type === "destination") {
+            let markerPosition = {
+              lat: this.nodes[i].destination.latitude,
+              lng: this.nodes[i].destination.longitude
+            };
+            singleMarker.destination = this.nodes[i];
+            singleMarker.position = markerPosition;
+            //add line here to give it a red marker if adjacent to a trip
+            //if (this.isAdjacentToTrip(i, this.nodes)) {
+            singleMarker.icon = this.blueMarker;
+            mapMarkers.push(singleMarker)
+          }
+        }
+        return mapMarkers;
+      }
 
 
     },
@@ -201,66 +247,66 @@
       //   }
       //   return tempPaths;
       // },
-      /**
-       * Creates an array of arrays containing latitude and longitude to represent different polylines.
-       * This is used to differentiate between different levels of nested trips.
-       */
-      pathList() {
-        let mapPath = [];
-        let currentDepth;
-        let Red = "#FF0000";
-        let Blue = "#4400FF";
-        for (let i = 0; i < this.nodes.length; i++) {
-          if (this.nodes[i].type === "destination") {
-            // currentDepth = this.destinations[i];
-            if (this.isAdjacentToTrip(i, this.nodes)) {
-              //could use Objects here if you want to name things
-              mapPath.push([this.nodes[i], Red]);
+      // /**
+      //  * Creates an array of arrays containing latitude and longitude to represent different polylines.
+      //  * This is used to differentiate between different levels of nested trips.
+      //  */
+      // pathList() {
+      //   let mapPath = [];
+      //   let currentDepth;
+      //   let Red = "#FF0000";
+      //   let Blue = "#4400FF";
+      //   for (let i = 0; i < this.nodes.length; i++) {
+      //     if (this.nodes[i].type === "destination") {
+      //       // currentDepth = this.destinations[i];
+      //       if (this.isAdjacentToTrip(i, this.nodes)) {
+      //         //could use Objects here if you want to name things
+      //         mapPath.push([this.nodes[i], Red]);
+      //
+      //         tempPath.strokeColor = "#" + RGB[0] + RGB[1] + RGB[2];
+      //         break
+      //        else if (i == 0) {
+      //         tempPath.push(this.destinations[i]);
+      //       } else if ((currentDepth < this.destinations[i + 1]) || (currentDepth < this.destinations[i - 1])) {
+      //         tempPath.push(this.destinations[i]);
+      //
+      //         if (currentDepth < this.destinations[i - 1]) {
+      //           RGB[((this.destinations[i - 1] + 2) % 3)] = "FF";
+      //         } else {
+      //           RGB[((this.destinations[i] + 2) % 3)] = "FF";
+      //         }
+      //         tempPath.strokeColor = "#" + RGB[0] + RGB[1] + RGB[2];
+      //         tempPaths.push(tempPath);
+      //         tempPath = [];
+      //         tempPath.push(this.destinations[i]);
+      //       } else {
+      //         tempPath.push(this.destinations[i]);
+      //       }
+      //     }
+      //
+      //   }
+      //   for (let i = 0; i < tempPaths.length; i++) {
+      //     tempPaths[i] = tempPaths[i].map((path) => {
+      //       return {
+      //         ...tempPaths[i],
+      //         lat: path.destination.latitude,
+      //         lng: path.destination.longitude,
+      //
+      //       }
+      //     });
+      //   }
+      //
+      //   if (this.destinations[0] != undefined) {
+      //     this.gMapOptions.center = {
+      //       lat: this.destinations[0].destination.latitude,
+      //       lng: this.destinations[0].destination.longitude
+      //     };
+      //   }
+      //   return tempPaths;
+      // }
 
-              tempPath.strokeColor = "#" + RGB[0] + RGB[1] + RGB[2];
-              break
-             else if (i == 0) {
-              tempPath.push(this.destinations[i]);
-            } else if ((currentDepth < this.destinations[i + 1]) || (currentDepth < this.destinations[i - 1])) {
-              tempPath.push(this.destinations[i]);
 
-              if (currentDepth < this.destinations[i - 1]) {
-                RGB[((this.destinations[i - 1] + 2) % 3)] = "FF";
-              } else {
-                RGB[((this.destinations[i] + 2) % 3)] = "FF";
-              }
-              tempPath.strokeColor = "#" + RGB[0] + RGB[1] + RGB[2];
-              tempPaths.push(tempPath);
-              tempPath = [];
-              tempPath.push(this.destinations[i]);
-            } else {
-              tempPath.push(this.destinations[i]);
-            }
-          }
-
-        }
-        for (let i = 0; i < tempPaths.length; i++) {
-          tempPaths[i] = tempPaths[i].map((path) => {
-            return {
-              ...tempPaths[i],
-              lat: path.destination.latitude,
-              lng: path.destination.longitude,
-
-            }
-          });
-        }
-
-        if (this.destinations[0] != undefined) {
-          this.gMapOptions.center = {
-            lat: this.destinations[0].destination.latitude,
-            lng: this.destinations[0].destination.longitude
-          };
-        }
-        return tempPaths;
-      }
-
-
-    },
+    // },
       isAdjacentToTrip(positionOfDestination, allNodes){
         let previousIndex = positionOfDestination - 1;
         let nextIndex = positionOfDestination + 1;
