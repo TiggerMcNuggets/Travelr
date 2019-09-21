5ed4e6ef1215b7e224cde57de7862df9c9fd7ada
Marvin Goesmann <mgo81@uclive.ac.nz>
2019-09-21 19:59:46 +1200

    S601 6581 adding ical to trip email

--- app/controllers/PopulateController.java
+++ app/controllers/PopulateController.java
@@ -1,11 +1,15 @@
 package controllers;
 
+import com.openhtmltopdf.pdfboxout.PdfRendererBuilder;
 import models.*;
 import play.mvc.Controller;
 import play.mvc.Result;
 
+import java.io.FileOutputStream;
+import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 public class PopulateController extends Controller {
     /**
