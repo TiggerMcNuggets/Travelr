cb2a321b73f78289e8437affe439d052f8f7a5f3
Dana Lambert <dla72@uclive.ac.nz>
2019-09-24 00:26:28 +1200

    Making login/signin screens responsive. #story[605]

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,52 +1,116 @@
 <template>
   <v-app id="inspire">
+    <div v-if="!isSmall && !isExtraSmall" class="home-container">
+      <img class="home__feature_img" src="../assets/homepage_graphic.png" />
+    </div>
     <v-container fluid fill-height ma-0 pa-0>
-      <v-layout row>
-        <v-flex col sm6 pa-5 ma-3 mt-5 ml-5>
-          <div class="heading-container">
-            <img class="home__logo" src="../assets/logo2_dark.png" />
-            <h3 mt-2 class="home__tagline">Business trips made easy</h3>
-          </div>
+      <v-layout row wrap d-flex direction="column" align-center>
+        <v-flex
+          col
+          sm12
+          md7
+          :class="!isSmall && !isExtraSmall ? 'pl-5 ml-5': 'ma-4' "
+        >
+          <v-flex :class="!isSmall && !isExtraSmall ? 'heading-container pt-3 pb-3': 'heading-container mt-5 mb-5'">
+            <img
+              v-if="isSmall || isExtraSmall"
+              class="home__mobile_feature_img"
+              src="../assets/homepage_graphic_mobile.png"
+            />
+
+            <div>
+              <img class="home__logo" src="../assets/logo2_dark.png" />
+              <h3 mt-2 class="home__tagline">Business trips made easy</h3>
+            </div>
+          </v-flex>
           <router-view class="home__login_area"></router-view>
-        </v-flex>
-        <v-flex col sm6>
-          <img class="home__feature_img" src="../assets/homepage_graphic.png" />
         </v-flex>
       </v-layout>
     </v-container>
   </v-app>
 </template>
 
-<style>
+<style lang="scss">
 @import url("https://fonts.googleapis.com/css?family=Roboto:300,400&display=swap");
 
-.home__logo {
-  width: 60%;
+@media screen and (max-width: 960px) {
+  .home__logo {
+    width: 90%;
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+  }
+
+  .home__mobile_feature_img {
+    width: 90%;
+    display: block;
+    margin: 50px auto;
+  }
+
+  .home__login_area {
+    width: 90%;
+    margin-left: auto;
+    margin-right: auto;
+    margin-bottom: 20px;
+
+    .login-button {
+      width: 100%;
+    }
+  }
+
+  .home__tagline {
+    font-size: 4vw;
+    text-align: center;
+  }
 }
 
-.home__feature_img {
-  width: 100%;
+@media screen and (min-width: 960px) {
+  .home__logo {
+    width: 60%;
+  }
+
+  .home__feature_img {
+    height: 100%;
+  }
+
+  .home__login_area {
+    width: 60%;
+
+    .login-button {
+      margin-bottom: 30px
+    }
+  }
+
+  .home-container {
+    position: fixed;
+    right: 0;
+    height: 100%;
+  }
+
+  .home__tagline {
+    font-size: 1.5vw;
+  }
 }
 
-.home__login_area {
-  width: 80%;
+.heading-container {
+  margin-top: 0px;
+  margin-bottom: 60px;
 }
 
 .home__tagline {
   font-family: "Roboto", sans-serif;
   color: lightslategray;
   font-weight: 300;
-  font-size: 20px;
-  margin-top: 15px;
-}
-
-.heading-container {
-  margin-bottom: 60px;
+  margin-top: 30px;
 }
 </style>
 
 <script>
+import DeviceSizeMixin from "../components/mixins/DeviceSizeMixin";
+
 export default {
+  mixins: [DeviceSizeMixin],
+
   name: "Home",
 
   data() {
